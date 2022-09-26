8cbc83cd8eb569f871a619fecd1bd5944ba17b3e
Handan Yolak <handanyolak@gmail.com>
2022-09-26 20:57:10 +0300

    Add footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,12 +2,14 @@
   <div>
     <TheHeader />
     <Nuxt />
+    <TheFooter />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import TheHeader from '~/components/layout/TheHeader.vue'
+import TheFooter from '~/components/layout/TheFooter.vue'
 
 export default defineComponent({
   setup() {},
