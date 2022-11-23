12c2062b1ac730a0f67da4c17f676bd90bb79811
handanyolak <handanyolak@gmail.com>
2022-11-23 20:57:36 +0300

    Move connect functions to userWallet.ts

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -4,13 +4,13 @@
       <span>TownyFi</span>
       <div class="space-x-2">
         <button
-          @click="connectWeb3()"
+          @click="connectWeb()"
           class="p-1 text-white rounded-sm bg-brown-dark"
         >
           Connect Wallet
         </button>
         <button
-          @click="disconnectWeb3()"
+          @click="disconnectWeb()"
           class="p-1 text-white rounded-sm bg-brown"
         >
           Disconnect Wallet
@@ -23,8 +23,7 @@
 
 <script lang="ts">
 import { storeToRefs } from 'pinia'
-import { defineComponent, ref, onMounted } from 'vue'
-import { ethers } from 'ethers'
+import { defineComponent, onMounted } from 'vue'
 import { useConnectionStore } from '~/stores/connection'
 import { useUserWalletStore } from '~/stores/userWallet'
 
@@ -35,103 +34,28 @@ export default defineComponent({
     const ethereum = connectionStore.ethereum
     const userWalletStore = useUserWalletStore()
     const { address, balance } = storeToRefs(userWalletStore)
-    const provider: any = ref()
-    if (ethereum) {
-      provider.value = new ethers.providers.Web3Provider(ethereum)
-    }
-    const isConnected = ref(false)
 
     // Hooks
     onMounted(async () => {
       if (ethereum) {
         //const accounts = await provider.listAccounts()
-        await connect()
+        await userWalletStore.connect()
       }
     })
 
     // Methods
-    const connectWeb3 = async () => {
-      try {
-        // if No web3 provider
-        if (!ethereum) {
-          throw new Error('No provider detected.')
-        }
-        // ask to connect
-        await provider.value.send('eth_requestAccounts', [])
 
-        await connect()
-      } catch (error) {
-        // user denied account access
-        console.log(error)
-      }
+    const connectWeb = () => {
+      userWalletStore.connectWeb3
     }
 
-    const connect = async () => {
-      // get address, balance etc.
-      await updateUserInfo()
-      // Check connecting
-
-      // TODO: remove this
-      isConnected.value = Boolean(address.value)
-      if (!isConnected.value) {
-        throw new Error('Connection Error.')
-      }
-      // started eth events
-      await startEthEvents()
+    const disconnectWeb = () => {
+      userWalletStore.disconnectWeb3
     }
 
-    const disconnectWeb3 = async () => {
-      // reset events
-      await stopEthEvents()
-      // set false to `isConnected`
-      isConnected.value = false
-      console.log('Disconnected.')
-    }
-
-    // get user address and balance
-    const updateUserInfo = async (_address = null) => {
-      await userWalletStore.updateUserAddress(_address)
-      await userWalletStore.updateUserBalance()
-    }
-
-    // start eth events
-    const startEthEvents = () => {
-      ethereum.on('chainChanged', handleChainChanged)
-      ethereum.on('accountsChanged', handleAccountsChanged)
-      ethereum.on('disconnect', handleDisconnect)
-    }
-
-    // stop eth events
-    const stopEthEvents = () => {
-      ethereum.removeListener('chainChanged', handleChainChanged)
-      ethereum.removeListener('accountsChanged', handleAccountsChanged)
-    }
-
-    // eth change chain event
-    const handleChainChanged = async () => {
-      await userWalletStore.updateUserBalance()
-      console.log('Chain has changed.')
-    }
-
-    // eth change account event
-    const handleAccountsChanged = async (_accounts: any) => {
-      if (_accounts.length > 0) {
-        await updateUserInfo(_accounts[0].toLowerCase())
-        console.log(`Linked account changed to '${_accounts[0]}'`)
-      } else {
-        console.log('Linked account not found. Page will be reloaded.')
-        await disconnectWeb3()
-      }
-    }
-
-    // eth disconnect event
-    const handleDisconnect = async () => {
-      await stopEthEvents()
-      isConnected.value = false
-    }
     return {
-      connectWeb3,
-      disconnectWeb3,
+      connectWeb,
+      disconnectWeb,
       balance,
       address,
       ethereum,
--- stores/userWallet.ts
+++ stores/userWallet.ts
@@ -6,18 +6,37 @@ export const useUserWalletStore = defineStore('userWalletStore', {
   state: () => ({
     address: '',
     balance: '',
+    isConnected: false,
   }),
   getters: {
     provider(state): any {
       return useConnectionStore().provider
     },
+    ethereum(): any {
+      return useConnectionStore().ethereum
+    },
   },
+
   actions: {
     setAddress(newAddress: string) {
       this.address = newAddress
     },
     setBalance(newBalance: string) {
       this.balance = newBalance
+    },
+    async connect() {
+      await this.updateUserInfo()
+
+      this.isConnected = Boolean(this.address)
+      if (!this.isConnected) {
+        throw new Error('Connection Error.')
+      }
+
+      await this.startEthEvents()
+    },
+    async updateUserInfo(_address = null) {
+      await this.updateUserAddress(_address)
+      await this.updateUserBalance()
     },
     async updateUserAddress(_address = null) {
       this.setAddress(
@@ -31,6 +50,55 @@ export const useUserWalletStore = defineStore('userWalletStore', {
           (await this.provider.getBalance(this.address)).toString()
         )
       )
+    },
+    startEthEvents() {
+      ethereum.on('chainChanged', this.handleChainChanged)
+      ethereum.on('accountsChanged', this.handleAccountsChanged)
+      ethereum.on('disconnect', this.handleDisconnect)
+    },
+    async handleChainChanged() {
+      await this.updateUserBalance()
+      console.log('Chain has changed.')
+    },
+    async handleAccountsChanged(_accounts: any) {
+      if (_accounts.length > 0) {
+        await this.updateUserInfo(_accounts[0].toLowerCase())
+        console.log(`Linked account changed to '${_accounts[0]}'`)
+      } else {
+        console.log('Linked account not found. Page will be reloaded.')
+        await this.disconnectWeb3()
+      }
+    },
+    stopEthEvents() {
+      ethereum.removeListener('chainChanged', this.handleChainChanged)
+      ethereum.removeListener('accountsChanged', this.handleAccountsChanged)
+    },
+    async handleDisconnect() {
+      await stopEthEvents()
+      this.isConnected = false
+    },
+
+    async disconnectWeb3() {
+      // reset events
+      await this.stopEthEvents()
+      // set false to `isConnected`
+      this.isConnected = false
+      console.log('Disconnected.')
+    },
+    async connectWeb3() {
+      try {
+        // if No web3 provider
+        if (!this.ethereum) {
+          throw new Error('No provider detected.')
+        }
+        // ask to connect
+        await this.provider.send('eth_requestAccounts', [])
+
+        await this.connect()
+      } catch (error) {
+        // user denied account access
+        console.log(error)
+      }
     },
   },
 })
