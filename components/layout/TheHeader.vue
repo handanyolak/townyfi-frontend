187ae31dc42193fc80d4b3e04c996d104f685147
handanyolak <handanyolak@gmail.com>
2022-11-27 08:56:52 +0300

    Add userRegister function and fix

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <div v-if="ethereum" class="flex justify-between py-3 my-5">
       <span>TownyFi</span>
-      <div class="space-x-2">
+      <div v-if="register" class="space-x-2">
         <button
           @click="connectWeb()"
           class="p-1 text-white rounded-sm bg-brown-dark"
@@ -16,12 +16,27 @@
           Disconnect Wallet
         </button>
       </div>
+      <button
+        v-else
+        @click="userRegister()"
+        class="p-1 text-white rounded-sm bg-brown"
+      >
+        Register the game
+      </button>
     </div>
-    <button v-else class="p-1 text-white rounded-sm bg-brown">install</button>
+    <a
+      v-else
+      href="https://metamask.io/download/"
+      target="_blank"
+      class="p-1 text-white rounded-sm bg-brown"
+    >
+      install
+    </a>
   </div>
 </template>
 
 <script lang="ts">
+import { ethers } from 'ethers'
 import { storeToRefs } from 'pinia'
 import { defineComponent, onMounted } from 'vue'
 import { useConnectionStore } from '~/stores/connection'
@@ -33,7 +48,9 @@ export default defineComponent({
     const connectionStore = useConnectionStore()
     const ethereum = connectionStore.ethereum
     const userWalletStore = useUserWalletStore()
-    const { address, balance } = storeToRefs(userWalletStore)
+    const kta = userWalletStore.kta
+    const ktaToken = userWalletStore.ktaToken
+    const { register } = storeToRefs(userWalletStore)
 
     // Hooks
     onMounted(async () => {
@@ -44,21 +61,32 @@ export default defineComponent({
     })
 
     // Methods
-
     const connectWeb = () => {
-      userWalletStore.connectWeb3
+      userWalletStore.connectWeb3()
     }
 
     const disconnectWeb = () => {
-      userWalletStore.disconnectWeb3
+      userWalletStore.disconnectWeb3()
+    }
+
+    const userRegister = async () => {
+      await (
+        await ktaToken
+          .connect(connectionStore.signer)
+          .approve(kta.address, ethers.constants.MaxUint256)
+      ).wait()
+
+      await kta
+        .connect(connectionStore.signer)
+        .register(ethers.constants.HashZero, ethers.constants.AddressZero)
     }
 
     return {
       connectWeb,
       disconnectWeb,
-      balance,
-      address,
+      userRegister,
       ethereum,
+      register,
     }
   },
 })
