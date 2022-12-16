77445d09a22116c0c628c9addb13c675e2787ca4
handanyolak <handanyolak@gmail.com>
2022-12-17 00:16:31 +0300

    Add GameInfo component

---
+++ components/GameInfo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>GAME INFO</div>
+</template>
+
+<script lang="ts">
+// TODO: ileride duzenlenecek
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup() {},
+})
+</script>
