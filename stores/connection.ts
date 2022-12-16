04f697767209627b61a367d1b6ba900374d74746
handanyolak <handanyolak@gmail.com>
2022-12-17 00:28:09 +0300

    Add new actions to connection.ts

--- stores/connection.ts
+++ stores/connection.ts
@@ -2,15 +2,31 @@ import { defineStore } from 'pinia'
 import { ethers, providers } from 'ethers'
 
 export const useConnectionStore = defineStore('connectionStore', {
+  state: () => ({
+    onValidNetwork: Boolean(window.ethereum) && window.ethereum.chainId == 5,
+    isConnected: false,
+  }),
   getters: {
+    hasMetamask(): boolean {
+      return Boolean(window.ethereum)
+    },
     ethereum(): any {
       return window.ethereum
     },
     provider(state): any {
-      if (this.ethereum) return new providers.Web3Provider(ethereum)
+      if (this.hasMetamask)
+        return new providers.Web3Provider(this.ethereum, 'any')
     },
     signer(state): any {
-      if (this.ethereum) return this.provider.getSigner()
+      if (this.hasMetamask) return this.provider.getSigner()
+    },
+  },
+  actions: {
+    setOnValidNetwork(newValue: boolean) {
+      this.onValidNetwork = this.hasMetamask && newValue
+    },
+    setIsConnected(newValue: boolean) {
+      this.isConnected = newValue
     },
   },
 })
