22d1c87371d821eaabbfc93a4443b88f75d8abe8
handanyolak <handanyolak@gmail.com>
2022-12-21 22:51:30 +0300

    Add sidebar component

---
+++ components/layout/TheSidebar.vue
@@ -0,0 +1,32 @@
+<template>
+  <Transition name="slide-fade">
+    <div
+      v-if="appOptionStore.showSidebar"
+      class="fixed top-0 z-50 min-h-screen bg-gray-400 w-60"
+      @mouseleave="sideLeave()"
+    >
+      <div>Sidebar</div>
+    </div>
+  </Transition>
+</template>
+
+<script setup lang="ts">
+const appOptionStore = useAppOptions()
+
+const sideLeave = () => {
+  appOptionStore.showSidebar = false
+}
+</script>
+
+<style>
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition-delay: 1s;
+  transition: transform 0.8s ease;
+}
+.slide-fade-enter,
+.slide-fade-leave-to {
+  transform: translateX(-100%);
+  transition: all 0.3s ease-out;
+}
+</style>
