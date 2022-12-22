b65dc2d1a75820c5e54d2d00be5228575261750b
handanyolak <handanyolak@gmail.com>
2022-12-23 01:32:10 +0300

    Add hover menu states

--- stores/appOptions.ts
+++ stores/appOptions.ts
@@ -1,6 +1,10 @@
 export const useAppOptions = defineStore('appOptionsStore', {
   state: () => ({
     showSidebar: false,
+    isGameInfo: false,
+    isWeb3Info: false,
+    isUserOptions: false,
+    isInteractions: false,
   }),
   getters: {},
   actions: {},
