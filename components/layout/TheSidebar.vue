8090c37dbe0578a70e8f5721a6d9c91ba1bb9257
handanyolak <handanyolak@gmail.com>
2022-12-23 01:31:28 +0300

    Add transitions conditions

--- components/layout/TheSidebar.vue
+++ components/layout/TheSidebar.vue
@@ -1,7 +1,18 @@
 <template>
-  <Transition name="slide-fade">
+  <Transition
+    :name="
+      appOptionStore.isWeb3Info || appOptionStore.isGameInfo
+        ? 'slide-left-fade'
+        : 'slide-right-fade'
+    "
+  >
     <div
       v-if="appOptionStore.showSidebar"
+      :class="
+        appOptionStore.isGameInfo || appOptionStore.isWeb3Info
+          ? 'left-0'
+          : 'right-0'
+      "
       class="fixed top-0 z-50 min-h-screen bg-gray-400 w-[500px] p-3"
       @mouseleave="sideLeave()"
     >
@@ -17,18 +28,33 @@ const appOptionStore = useAppOptions()
 
 const sideLeave = () => {
   appOptionStore.showSidebar = false
+  appOptionStore.isGameInfo = false
+  appOptionStore.isWeb3Info = false
+  appOptionStore.isUserOptions = false
+  appOptionStore.isInteractions = false
 }
 </script>
 
 <style>
-.slide-fade-enter-active,
-.slide-fade-leave-active {
+.slide-left-fade-enter-active,
+.slide-left-fade-leave-active {
   transition-delay: 1s;
   transition: transform 0.8s ease;
 }
-.slide-fade-enter,
-.slide-fade-leave-to {
+.slide-left-fade-enter,
+.slide-left-fade-leave-to {
   transform: translateX(-100%);
+  transition: all 0.3s ease-out;
+}
+
+.slide-right-fade-enter-active,
+.slide-right-fade-leave-active {
+  transition-delay: 1s;
+  transition: transform 0.8s ease;
+}
+.slide-right-fade-enter,
+.slide-right-fade-leave-to {
+  transform: translateX(100%);
   transition: all 0.3s ease-out;
 }
 </style>
