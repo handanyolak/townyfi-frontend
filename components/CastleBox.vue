672ce79b901b1960fb72f4cd6bf5e77dffbc4a12
handanyolak <handanyolak@gmail.com>
2022-12-27 01:08:56 +0300

    Fix castle box style

--- components/CastleBox.vue
+++ components/CastleBox.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="castle relative flex h-[350px] w-[350px] flex-col items-center rounded-xl text-2xl font-bold shadow-2xl dark:shadow-lg dark:shadow-slate-400"
+    class="castle relative flex min-h-[350px] flex-col items-center rounded-xl text-2xl font-bold shadow-2xl dark:shadow-lg dark:shadow-slate-400"
   >
     <div>
       <span>{{ item.x }}</span>
@@ -9,18 +9,18 @@
     </div>
     <button @click="toggleModal()">
       <img
-        class="visible absolute bottom-0 z-10 h-10 w-10 dark:invisible"
+        class="visible absolute bottom-12 z-10 h-10 w-10 dark:invisible"
         src="@/assets/img/knight.svg"
         alt="soldier"
       />
       <img
-        class="invisible absolute bottom-0 z-10 h-10 w-10 dark:visible"
+        class="invisible absolute bottom-12 z-10 h-10 w-10 dark:visible"
         src="@/assets/img/soldier.svg"
         alt="soldier"
       />
     </button>
 
-    <CastleSvg class="h-full w-full p-11"></CastleSvg>
+    <CastleSvg class="absolute h-full w-full p-10"></CastleSvg>
   </div>
 </template>
 
