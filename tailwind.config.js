1c590cfb607f4e702894325286f823bbb3a7d63d
handanyolak <handanyolak@gmail.com>
2022-12-27 01:09:18 +0300

    Fix padding

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,9 @@ module.exports = {
     extend: {
       container: {
         center: true,
+        padding: {
+          DEFAULT: '6rem',
+        },
       },
       colors: {
         brown: {
