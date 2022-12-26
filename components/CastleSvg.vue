0eaeac5766080f2a85b08949d439978350163083
handanyolak <handanyolak@gmail.com>
2022-12-27 01:21:52 +0300

    Fix width and height

--- components/CastleSvg.vue
+++ components/CastleSvg.vue
@@ -1,8 +1,8 @@
 <template>
   <svg
     id="Layer_1"
-    height="800px"
-    width="800px"
+    height="400px"
+    width="400px"
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
     xmlns:xlink="http://www.w3.org/1999/xlink"
