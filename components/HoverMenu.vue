d8311af54480a1d41d27b744bde7cd230a87a614
handanyolak <handanyolak@gmail.com>
2022-12-27 01:22:26 +0300

    Fix prettier errors

--- components/HoverMenu.vue
+++ components/HoverMenu.vue
@@ -1,48 +1,48 @@
 <template>
   <div>
     <div
-      class="absolute left-0 p-2 transition ease-in-out rounded-r-lg top-40 pl-7 bg-amber-900"
+      class="absolute left-0 top-40 rounded-r-lg bg-amber-900 p-2 pl-7 transition ease-in-out"
       :class="
         appOptionStore.isGameInfo
           ? 'translate-x-0'
-          : 'duration-1000 -translate-x-6'
+          : '-translate-x-6 duration-1000'
       "
       @mouseover="sideOver('isGameInfo')"
     >
-      <img src="@/assets/img/flag.svg" class="w-10 h-10" />
+      <img src="@/assets/img/flag.svg" class="h-10 w-10" />
     </div>
     <div
-      class="absolute left-0 p-2 transition ease-in-out rounded-r-lg top-80 pl-7 bg-amber-900"
+      class="absolute left-0 top-80 rounded-r-lg bg-amber-900 p-2 pl-7 transition ease-in-out"
       :class="
         appOptionStore.isWeb3Info
           ? 'translate-x-0'
-          : 'duration-1000 -translate-x-6'
+          : '-translate-x-6 duration-1000'
       "
       @mouseover="sideOver('isWeb3Info')"
     >
-      <img src="@/assets/img/flag.svg" class="w-10 h-10" />
+      <img src="@/assets/img/flag.svg" class="h-10 w-10" />
     </div>
     <div
-      class="absolute right-0 p-2 transition ease-in-out rounded-l-lg top-40 pr-7 bg-amber-900"
+      class="absolute right-0 top-40 rounded-l-lg bg-amber-900 p-2 pr-7 transition ease-in-out"
       :class="
         appOptionStore.isUserOptions
           ? 'translate-x-0'
-          : 'duration-1000 translate-x-6'
+          : 'translate-x-6 duration-1000'
       "
       @mouseover="sideOver('isUserOptions')"
     >
-      <img src="@/assets/img/flag.svg" class="w-10 h-10" />
+      <img src="@/assets/img/flag.svg" class="h-10 w-10" />
     </div>
     <div
-      class="absolute right-0 p-2 transition ease-in-out rounded-l-lg top-80 pr-7 bg-amber-900"
+      class="absolute right-0 top-80 rounded-l-lg bg-amber-900 p-2 pr-7 transition ease-in-out"
       :class="
         appOptionStore.isInteractions
           ? 'translate-x-0'
-          : 'duration-1000 translate-x-6'
+          : 'translate-x-6 duration-1000'
       "
       @mouseover="sideOver('isInteractions')"
     >
-      <img src="@/assets/img/flag.svg" class="w-10 h-10" />
+      <img src="@/assets/img/flag.svg" class="h-10 w-10" />
     </div>
   </div>
 </template>
