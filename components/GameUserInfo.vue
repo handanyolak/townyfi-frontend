6ad33b7c8863c51d5deef4b05b5e5f706adf0e24
handanyolak <handanyolak@gmail.com>
2023-01-07 07:21:36 +0300

    Create convert function

--- components/GameUserInfo.vue
+++ components/GameUserInfo.vue
@@ -17,6 +17,10 @@
         <template #list-title> Referrer </template>
         <span>{{ referrer }}</span>
       </ContentListItem>
+      <ContentListItem convertable @convert="convert()">
+        <template #list-title> Health </template>
+        <span>{{ health }}</span>
+      </ContentListItem>
     </div>
   </div>
 </template>
@@ -28,11 +32,17 @@ import ContentListItem from '~/components/sidebar-items/ContentListItem.vue'
 const address = ref('0xB55F8FC6de35c643a2Ed462d3316706A4159D41D')
 const name = ref('Hadalobo')
 const coordinate = ref('(0,1)')
+const health = ref(23237438573957)
+const healthTime = ref(24)
 
 const referrer = computed(
   () =>
-    address.value.substring(0, 7) +
+    address.value.substring(0, 5) +
     '...' +
     address.value.substring(address.value.length - 5)
 )
+
+const convert = () => {
+  health.value = healthTime.value
+}
 </script>
