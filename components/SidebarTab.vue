b1827b49d68b9b3e2bee7cb1a060a173efb631c7
handanyolak <handanyolak@gmail.com>
2023-01-08 16:42:01 +0300

    Add dynamic components

--- components/SidebarTab.vue
+++ components/SidebarTab.vue
@@ -1,81 +1,42 @@
 <template>
-  <div style="font-family: 'Bilbo Swash Caps'" class="mx-4 p-4">
+  <div class="mx-4 p-4">
     <ul class="my-3 grid grid-cols-2 space-x-2">
-      <li class="text-center">
+      <li v-for="(tab, index) in tabs" :key="index" class="text-center">
         <a
           style="font-family: Pirata One, sans-serif"
-          class="block rounded p-2 font-bold uppercase leading-normal shadow-lg"
-          :class="{
-            'bg-towni-brown-100 text-towni-brown-200 hover:shadow-towni-brown-300':
-              openTab !== 1,
-            'bg-towni-brown-200 text-towni-brown-100 ': openTab === 1,
-          }"
-          @click="toggleTabs(1)"
+          :class="[
+            'block cursor-pointer rounded p-2 font-bold uppercase leading-normal shadow-lg',
+            currentTab === tab
+              ? 'bg-towni-brown-200 text-towni-brown-100 '
+              : 'bg-towni-brown-100 text-towni-brown-200 hover:shadow-towni-brown-300',
+          ]"
+          @click="changeTab(tab)"
         >
-          User
-        </a>
-      </li>
-      <li class="text-center">
-        <a
-          style="font-family: Pirata One, sans-serif"
-          class="block rounded p-2 font-bold uppercase leading-normal shadow-lg"
-          :class="{
-            'bg-towni-brown-100 text-towni-brown-200 hover:shadow-towni-brown-300':
-              openTab !== 2,
-            'bg-towni-brown-200 text-towni-brown-100': openTab === 2,
-          }"
-          @click="toggleTabs(2)"
-        >
-          Town
+          {{ tab }}
         </a>
       </li>
     </ul>
     <div class="w-full">
-      <div>
-        <div class="tab-content tab-space">
-          <div :class="{ hidden: openTab !== 1, block: openTab === 1 }">
-            <p>
-              Collaboratively administrate empowered markets via plug-and-play
-              networks. Dynamically procrastinate B2C users after installed base
-              benefits.
-              <br />
-              <br />
-              Dramatically visualize customer directed convergence without
-              revolutionary ROI.
-            </p>
-          </div>
-          <div :class="{ hidden: openTab !== 2, block: openTab === 2 }">
-            <p>
-              Completely synergize resource taxing relationships via premier
-              niche markets. Professionally cultivate one-to-one customer
-              service with robust ideas.
-              <br />
-              <br />
-              Dynamically innovate resource-leveling customer service for state
-              of the art customer service.
-            </p>
-          </div>
-          <div :class="{ hidden: openTab !== 3, block: openTab === 3 }">
-            <p>
-              Efficiently unleash cross-media information without cross-media
-              value. Quickly maximize timely deliverables for real-time schemas.
-              <br />
-              <br />
-              Dramatically maintain clicks-and-mortar solutions without
-              functional solutions.
-            </p>
-          </div>
-        </div>
-      </div>
+      <Component :is="currentComponent" class="tab"></Component>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const openTab = ref(1)
+interface ContentListItemProps {
+  tabs: string[]
+}
+const props = defineProps<ContentListItemProps>()
+const currentTab = ref(props.tabs[0])
 
-const toggleTabs = (tabNumber: number) => {
-  openTab.value = tabNumber
+const currentComponent = computed(() => {
+  const _currentTab = currentTab.value
+
+  return defineAsyncComponent(() => import(`/components/${_currentTab}.vue`))
+})
+
+const changeTab = (tab: string) => {
+  currentTab.value = tab
 }
 </script>
 
