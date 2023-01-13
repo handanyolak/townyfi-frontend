5aa13499d7a15427a149c14cd8a0aacf952b4783
handanyolak <handanyolak@gmail.com>
2023-01-13 16:13:48 +0300

    Create button component

---
+++ components/TownyButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <button
+    :class="[
+      'rounded-md border-2 border-solid border-transparent p-2 text-sm   transition-colors duration-200 ease-in-out ',
+      border
+        ? 'border-gradient hover:fill-gradient text-towni-brown-200 hover:text-white'
+        : 'fill-gradient hover:border-gradient text-white hover:text-towni-brown-200',
+    ]"
+  >
+    <slot />
+  </button>
+</template>
+
+<script setup lang="ts">
+interface ButtonThemeProps {
+  fill?: boolean
+  border?: boolean
+}
+defineProps<ButtonThemeProps>()
+</script>
