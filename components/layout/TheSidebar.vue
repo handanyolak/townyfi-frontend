7753a3258f32c629fc792310b5ae79cd18215f02
handanyolak <handanyolak@gmail.com>
2023-01-16 17:18:15 +0300

    Fix timing bug

--- components/layout/TheSidebar.vue
+++ components/layout/TheSidebar.vue
@@ -1,62 +1,62 @@
 <template>
-  <Transition
-    :name="isGameInfo || isWeb3Info ? 'slide-left-fade' : 'slide-right-fade'"
-  >
-    <div
-      v-if="appOptionStore.showSidebar"
-      :class="[
-        'fixed top-0 z-50 min-h-screen w-[500px]',
-        isGameInfo || isWeb3Info ? '-left-3' : '-right-3',
-      ]"
-      @mouseleave="sideLeave()"
+  <div>
+    <Transition
+      :name="isGameInfo || isWeb3Info ? 'slide-left-fade' : 'slide-right-fade'"
     >
-      <div>
-        <div
-          :class="[
-            'parchment absolute -top-3 w-full bg-white [filter:url(#wavy)]',
-            appOptionStore.isUserOptions || appOptionStore.isInteractions
-              ? 'rotate-180 transform'
-              : '',
-          ]"
-        ></div>
-        <div class="relative">
-          <SidebarTabVue :tabs="tabs" />
+      <div
+        v-if="showSidebar"
+        :class="[
+          'fixed -top-3 z-50 min-h-screen w-96',
+          isGameInfo || isWeb3Info ? '-left-4' : '-right-4',
+        ]"
+        @mouseleave="sideLeave()"
+      >
+        <div>
+          <div
+            :class="[
+              'parchment absolute -top-3 w-full bg-white [filter:url(#wavy)]',
+              isUserOptions || isInteractions ? 'rotate-180 transform' : '',
+            ]"
+          ></div>
+          <div class="relative">
+            <SidebarTabVue :tabs="tabs" />
+          </div>
+          <svg>
+            <filter id="wavy">
+              <feTurbulence
+                x="0"
+                y="0"
+                baseFrequency="0.02"
+                numOctaves="5"
+                seed="1"
+              />
+              <feDisplacementMap in="SourceGraphic" scale="40" />
+            </filter>
+          </svg>
         </div>
-
-        <svg>
-          <filter id="wavy">
-            <feTurbulence
-              x="0"
-              y="0"
-              baseFrequency="0.02"
-              numOctaves="5"
-              seed="1"
-            />
-            <feDisplacementMap in="SourceGraphic" scale="40" />
-          </filter>
-        </svg>
       </div>
-    </div>
-  </Transition>
+    </Transition>
+  </div>
 </template>
 
 <script setup lang="ts">
 import SidebarTabVue from '@/components/SidebarTab.vue'
 
 const appOptionStore = useAppOptions()
-const { isGameInfo, isWeb3Info } = storeToRefs(appOptionStore)
+const { isGameInfo, isWeb3Info, isUserOptions, isInteractions, showSidebar } =
+  storeToRefs(appOptionStore)
+
 const tabs = ['User', 'Town']
 
 const sideLeave = () => {
-  console.log(isWeb3Info.value)
-  console.log(isGameInfo.value)
-  appOptionStore.showSidebar = false
-  isGameInfo.value = false
-  isWeb3Info.value = false
-  appOptionStore.isUserOptions = false
-  appOptionStore.isInteractions = false
-  console.log(isWeb3Info.value)
-  console.log(isGameInfo.value)
+  showSidebar.value = false
+
+  requestAnimationFrame(() => {
+    isGameInfo.value = false
+    isWeb3Info.value = false
+    isUserOptions.value = false
+    isInteractions.value = false
+  })
 }
 </script>
 
