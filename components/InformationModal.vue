20a539b4a0412f1e9b2accca02b70429ed08b30a
handanyolak <handanyolak@gmail.com>
2023-01-20 22:10:57 +0300

    Fix information modal style and Add content classes props

--- components/InformationModal.vue
+++ components/InformationModal.vue
@@ -1,50 +1,34 @@
 <template>
-  <div>
-    <Teleport to="body">
-      <Transition
-        enter-active-class="transition duration-200 ease-out transform"
-        enter-from-class="opacity-0"
-        enter-to-class="opacity-100"
-        leave-active-class="transition duration-200 ease-in transform"
-        leave-from-class="opacity-100"
-        leave-to-class="opacity-0"
-      >
+  <Teleport to="body">
+    <div class="fixed inset-0 z-50 h-full w-full bg-black bg-opacity-20">
+      <div class="flex h-screen items-center justify-center">
         <div
-          class="modal-backdrop fixed inset-0 h-full w-full bg-black bg-opacity-20"
+          ref="modal"
+          :class="contentClasses"
+          class="modal relative min-h-[20%] min-w-[30%] rounded-lg bg-white"
         >
-          <div class="flex justify-center pt-24">
-            <Transition
-              enter-active-class="transition duration-300 ease-out transform"
-              enter-from-class="scale-95 translate-y-10 opacity-0"
-              enter-to-class="scale-100 translate-y-10 opacity-100"
-              leave-active-class="duration-200 tease-in"
-              leave-from-class="scale-100 translate-y-0 opacity-100"
-              leave-to-class="scale-95 translate-y-0 translate-y-10 opacity-0"
-            >
-              <div
-                ref="modal"
-                class="modal relative w-1/2 rounded-lg bg-white p-4 shadow-xl"
-                role="dialog"
-              >
-                <button class="absolute right-2 top-2" @click="closeModal">
-                  <img class="h-5 w-5" src="@/assets/img/exit.svg" />
-                </button>
-                <slot />
-              </div>
-            </Transition>
-          </div>
+          <button class="absolute right-2 top-2" @click="closeModal">
+            <img class="h-10 w-10" src="@/assets/img/exit.svg" />
+          </button>
+          <slot />
         </div>
-      </Transition>
-    </Teleport>
-  </div>
+      </div>
+    </div>
+  </Teleport>
 </template>
 
 <script setup lang="ts">
 import { onClickOutside } from '@vueuse/core'
 
-const modal = ref(null)
+interface ContentClassesProps {
+  contentClasses: string
+}
+
+defineProps<ContentClassesProps>()
 
 const emit = defineEmits(['modalClosed'])
+
+const modal = ref(null)
 
 const closeModal = () => {
   emit('modalClosed')
