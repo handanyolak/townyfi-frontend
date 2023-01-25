0e61f5be26df062e89dac7668cae8f34870e98e6
handanyolak <handanyolak@gmail.com>
2023-01-25 21:22:06 +0300

    fix style

--- components/layout/TheSidebar.vue
+++ components/layout/TheSidebar.vue
@@ -12,8 +12,8 @@
     >
       <img
         :class="[
-          'absolute top-2  z-10 h-10 w-10 cursor-pointer',
-          isGameInfo || isWeb3Info ? '-right-2' : '-left-2',
+          'absolute top-3  z-10 h-8 w-8 cursor-pointer',
+          isGameInfo || isWeb3Info ? '-right-1' : '-left-2',
         ]"
         src="@/assets/img/exit.svg"
         @click="sideLeave()"
