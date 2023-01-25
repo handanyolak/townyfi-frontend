d0825b6d67cadd8f243ba01511f83f5eda54eb34
handanyolak <handanyolak@gmail.com>
2023-01-25 21:20:28 +0300

    Delete TheFooter import

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,14 +1,12 @@
 <template>
-  <div class="min-h-screen bg-towni-brown-light-100 dark:bg-night-blue">
+  <div class="min-h-screen bg-towni-brown-light-100 dark:bg-[#040413]">
     <TheHeader />
     <TheSidebar />
     <slot />
-    <TheFooter />
   </div>
 </template>
 
 <script setup lang="ts">
 import TheHeader from '~/components/layout/TheHeader.vue'
 import TheSidebar from '~/components/layout/TheSidebar.vue'
-import TheFooter from '~/components/layout/TheFooter.vue'
 </script>
