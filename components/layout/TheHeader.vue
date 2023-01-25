f6629621c61660b49e3c951f8541e01b55f42b7e
handanyolak <handanyolak@gmail.com>
2023-01-25 21:07:01 +0300

    Fix header condition level and styles

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -1,77 +1,69 @@
 <template>
   <div class="container">
-    <div class="space-x-2"></div>
-    <div v-if="hasMetamask" class="flex justify-between py-5">
-      <div>
-        <span
-          class="flex items-center bg-gradient-to-r from-towni-brown-dark-400 to-towni-brown-dark-200 bg-clip-text text-5xl font-extrabold text-transparent"
-          :class="isDark ? 'text-white' : ''"
-          style="font-family: Pirata One, sans-serif"
-        >
-          <img class="h-7 w-7" src="@/assets/img/paper-document.svg" />
-          TownyFi
-        </span>
-      </div>
-      <div v-if="onValidNetwork">
-        <div v-if="isConnected">
-          <div class="flex items-center space-x-2">
-            <TownyButton
-              v-if="!isRegistered"
-              fill
-              @click.native="userRegister()"
-            >
-              Register the game
-            </TownyButton>
-            <TownyButton fill @click.native="disconnectWeb3()">
-              {{ $t('disconnect_wallet') }}
-            </TownyButton>
-            <LanguageDropdown />
-            <div>
-              <img
-                :src="themeIcon"
-                class="h-5 w-5 cursor-pointer"
-                @click="toggleTheme()"
-              />
-            </div>
-            <div>
-              <img
-                :src="audioIcon"
-                class="h-7 w-7 cursor-pointer"
-                @click="toggleAudio()"
-              />
-            </div>
-            <div>
-              <img
-                v-if="audio"
-                :src="musicIcon"
-                class="h-5 w-5 cursor-pointer"
-                @click="toggleMusic()"
-              />
-            </div>
-          </div>
-        </div>
-        <TownyButton v-else border @click.native="connectWeb3()">
-          Connect Wallet
-        </TownyButton>
-      </div>
-
-      <TownyButton
-        v-else
-        target="_blank"
-        border
-        @click.native="switchNetwork()"
+    <div class="flex justify-between py-5">
+      <span
+        class="flex items-center bg-gradient-to-r from-towni-brown-dark-400 via-towni-brown-dark-400 to-towni-brown-dark-200 bg-clip-text text-5xl font-extrabold text-transparent"
+        :class="isDark ? 'text-white' : ''"
+        style="font-family: Pirata One, sans-serif"
       >
-        Switch Network
-      </TownyButton>
+        <img class="h-7 w-7" src="@/assets/img/paper-document.svg" />
+        TownyFi
+      </span>
+      <div class="flex items-center space-x-2">
+        <div v-if="hasMetamask" class="flex justify-between py-5">
+          <div v-if="onValidNetwork">
+            <div v-if="isConnected">
+              <TownyButton
+                v-if="!isRegistered"
+                fill
+                @click.native="userRegister()"
+              >
+                Register the game
+              </TownyButton>
+              <TownyButton fill @click.native="disconnectWeb3()">
+                {{ $t('disconnect_wallet') }}
+              </TownyButton>
+            </div>
+            <TownyButton v-else border @click.native="connectWeb3()">
+              Connect Wallet
+            </TownyButton>
+          </div>
+          <TownyButton
+            v-else
+            target="_blank"
+            border
+            @click.native="switchNetwork()"
+          >
+            Switch Network
+          </TownyButton>
+        </div>
+        <TownyButton
+          v-else
+          href="https://metamask.io/download/"
+          target="_blank"
+          border
+        >
+          Install Metamask
+        </TownyButton>
+        <LanguageDropdown />
+        <img
+          :src="themeIcon"
+          class="h-5 w-5 cursor-pointer"
+          @click="toggleTheme()"
+        />
+        <img
+          :src="audioIcon"
+          class="h-7 w-7 cursor-pointer"
+          @click="toggleAudio()"
+        />
+        <img
+          v-if="audio"
+          :src="musicIcon"
+          class="h-5 w-5 cursor-pointer"
+          @click="toggleMusic()"
+        />
+      </div>
     </div>
-    <TownyButton
-      v-else
-      href="https://metamask.io/download/"
-      target="_blank"
-      border
-    >
-      install
-    </TownyButton>
   </div>
 </template>
 
