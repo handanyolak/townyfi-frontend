3cd4919c6926c9efa405d5419ce30af5679d4f89
handanyolak <handanyolak@gmail.com>
2023-01-26 23:36:11 +0300

    Add tooltip image slote and auto close props

--- components/Tooltip.vue
+++ components/Tooltip.vue
@@ -1,15 +1,13 @@
 <template>
-  <span class="relative" @click="toggleTooltip()">
-    <img
-      class="mr-1 h-3 w-3 cursor-pointer opacity-0 transition-opacity duration-500 ease-out group-hover:opacity-100"
-      src="@/assets/img/information.svg"
-    />
+  <span ref="tooltip" class="relative" @click="toggleTooltip()">
+    <slot name="tooltip-image" />
     <span
       :class="[
-        'absolute bottom-4 w-64 transform rounded-md bg-towni-brown-light-400 py-1 text-center text-sm text-towni-brown-dark-300 shadow-xl transition-all duration-700 ease-out',
+        ' absolute bottom-4 transform rounded-md bg-towni-brown-light-400 p-1 py-1 text-center text-sm text-towni-brown-dark-300 shadow-xl transition-all duration-700 ease-out',
         isShowTooltip
           ? 'visible translate-y-0 transform cursor-pointer bg-towni-brown-light-200'
           : 'invisible -translate-y-12',
+        autoClose ? '' : 'w-56',
       ]"
     >
       <slot />
@@ -18,9 +16,22 @@
 </template>
 
 <script setup lang="ts">
+import { onClickOutside } from '@vueuse/core'
 const isShowTooltip = ref(false)
+const tooltip = ref(null)
 
+const props = defineProps({
+  autoClose: {
+    type: Boolean,
+    default: false,
+  },
+})
+console.log(props.autoClose)
+onClickOutside(tooltip, () => (isShowTooltip.value = false))
 const toggleTooltip = () => {
   isShowTooltip.value = !isShowTooltip.value
+  if (props.autoClose) {
+    setTimeout(() => (isShowTooltip.value = false), 1500)
+  }
 }
 </script>
