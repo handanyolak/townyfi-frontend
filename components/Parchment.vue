99270850b83fb053f9bbd85b9d089dda36804fa8
handanyolak <handanyolak@gmail.com>
2023-01-29 05:33:17 +0300

    Add parchment title slot & Fix styles

--- components/Parchment.vue
+++ components/Parchment.vue
@@ -1,9 +1,11 @@
 <template>
   <div>
     <div
-      class="round-parchment h-20 -scale-105 transform rounded-xl bg-white"
-    ></div>
-    <div class="parchment bg-white text-towni-brown-dark-600">
+      class="round-parchment h-20 scale-105 transform rounded-xl bg-white py-3 text-center text-5xl font-bold text-towni-brown-dark-400"
+    >
+      <slot name="parchment-title" />
+    </div>
+    <div class="parchment bg-white py-8 px-2 text-towni-brown-dark-600">
       <div class="animate-fadein">
         <slot />
       </div>
@@ -15,8 +17,10 @@
 </template>
 
 <style scoped>
+@import url('https://fonts.googleapis.com/css2?family=Bilbo&display=swap');
 .round-parchment {
   box-shadow: 2px 3px 20px #3f2007, 0px -10px 40px #8a4d0f inset;
+  font-family: 'Bilbo', cursive;
 }
 .parchment {
   height: 40vh;
