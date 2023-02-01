3b74124f6463db50c2664c3f15db303edbffa339
handanyolak <handanyolak@gmail.com>
2023-02-01 16:10:34 +0300

    Add rounded shadow

--- components/CastleBox.vue
+++ components/CastleBox.vue
@@ -1,24 +1,32 @@
 <template>
   <div
-    class="relative rounded-xl bg-[#fffcf8] p-5 shadow-lg shadow-towni-brown-light-200 dark:bg-night-blue dark:shadow-sky-900"
+    class="relative rounded-xl bg-[#fffcf8] p-5 shadow-lg shadow-towni-brown-light-200 duration-300 dark:bg-night-blue dark:shadow-sky-900"
   >
-    <div class="flex justify-between">
+    <div class="absolute -top-5 flex w-full justify-between">
       <div
-        class="bg-gradient-to-b from-[#bf841a] via-[#bf841a] to-[#ffcd74] bg-clip-text text-sm text-transparent"
+        class="flex items-center justify-center bg-gradient-to-b from-[#bf841a] via-[#bf841a] to-[#ffcd74] bg-clip-text text-sm text-transparent"
       >
-        <span>({{ item.x.toString() }}</span>
-        <span>,</span>
-        <span>{{ item.y.toString() }})</span>
+        <div>
+          <span>({{ item.x.toString() }}</span>
+          <span>,</span>
+          <span>{{ item.y.toString() }})</span>
+        </div>
       </div>
-      <span
-        :class="{
-          'bg-green-500': item.addresses.length <= 10,
-          'bg-yellow-500':
-            item.addresses.length > 10 && item.addresses.length < 1000,
-          'bg-red-500': item.addresses.length >= 1000,
-        }"
-        class="inline-block h-1 w-1 rounded-full p-1"
-      ></span>
+
+      <div
+        class="rounded-shadow flex h-20 w-20 items-center justify-center rounded-full"
+      >
+        <span
+          :class="{
+            'bg-green-500': item.addresses.length <= 10,
+            'bg-yellow-500':
+              item.addresses.length > 10 && item.addresses.length < 1000,
+            'bg-red-500': item.addresses.length >= 1000,
+          }"
+          class="inline-block h-1 w-1 rounded-full p-1"
+        >
+        </span>
+      </div>
     </div>
     <div
       v-if="
@@ -29,28 +37,28 @@
     >
       <div
         :class="[
-          'arrow top-[1%] left-[50%] mx-5 -rotate-90',
+          'arrow top-[1%] left-[50%] z-10 mx-5 -rotate-90',
           isMove === 0 ? 'w-16' : 'w-4',
         ]"
         @click="userMove(Direction.Up)"
       ></div>
       <div
         :class="[
-          'arrow top-[50%] -right-[1%] my-5 rotate-0',
+          'arrow top-[50%] left-[99%] z-10 my-5 rotate-0',
           isMove === 1 ? 'w-16' : 'w-4',
         ]"
         @click="userMove(Direction.Right)"
       ></div>
       <div
         :class="[
-          'arrow bottom-[2%] left-[50%] mx-5 rotate-90',
+          'arrow bottom-[2%] left-[50%] z-10 mx-5 rotate-90',
           isMove === 2 ? 'w-16' : 'w-4',
         ]"
         @click="userMove(Direction.Down)"
       ></div>
       <div
         :class="[
-          'arrow top-[50%] left-[2%] my-5 rotate-180',
+          'arrow top-[50%] left-[2%] z-10 my-5 rotate-180',
           isMove === 3 ? 'w-16' : 'w-4',
         ]"
         @click="userMove(Direction.Left)"
@@ -115,5 +123,13 @@ const userMove = async (direction: Direction) => {
 <style>
 .arrow {
   @apply absolute h-2  origin-left transform cursor-pointer bg-gradient-to-tl from-towni-brown-dark-200  to-towni-brown-light-400 transition-all duration-300 ease-in-out before:absolute before:-right-3 before:-top-2 before:bottom-0 before:h-0 before:w-0 before:border-t-12 before:border-b-12 before:border-l-18 before:border-towni-brown-dark-100 before:border-b-transparent before:border-t-transparent;
+}
+
+.rounded-shadow {
+  background: radial-gradient(
+    circle,
+    hsla(0, 0%, 83.9%, 0.3) 0,
+    hsla(0, 0%, 90.6%, 0) 30%
+  );
 }
 </style>
