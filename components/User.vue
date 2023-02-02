6b1cde39c155c32bd94cb68ae6352e4c6575cbee
handanyolak <handanyolak@gmail.com>
2023-02-02 16:55:30 +0300

    Add real data & Add convert and max string functions

--- components/User.vue
+++ components/User.vue
@@ -4,23 +4,33 @@
     <ListItem editable>
       <template #title> Name: </template>
       <template #input>
-        <input v-model="userName" type="text" />
+        <div class="relative">
+          <input v-model="stringName" type="text" @keyup="maxString()" />
+          <div
+            :class="[
+              'absolute w-full bg-towni-brown-dark-500 text-[11px] transition-all duration-1000 ease-in-out',
+              nameError ? 'max-h-10' : 'max-h-0',
+            ]"
+          >
+            {{ nameError }}
+          </div>
+        </div>
       </template>
-      <span>{{ userName }}</span>
+      <span>{{ ethers.utils.parseBytes32String(user.name as any) }}</span>
     </ListItem>
     <ListItem>
       <template #title> Coordinate </template>
-      <span>({{ userGameStore.user.coordinate._x.toString() }}</span>
+      <span>({{ user.coordinate._x.toString() }}</span>
       <span>,</span>
-      <span>{{ userGameStore.user.coordinate._y.toString() }})</span>
+      <span>{{ user.coordinate._y.toString() }})</span>
     </ListItem>
     <ListItem>
       <template #title> Level: </template>
-      <span>{{ userGameStore.user.levelId }}</span>
+      <span>{{ user.levelId }}</span>
     </ListItem>
     <ListItem>
       <template #title> Exp: </template>
-      <span>{{ userGameStore.user.exp }}</span>
+      <span>{{ user.exp }}</span>
     </ListItem>
     <ListItem copiable :copy-value="reffererAddress">
       <template #title> Referrer: </template>
@@ -29,106 +39,60 @@
     <ListTitle>Stats</ListTitle>
     <ListItem>
       <template #title> Health: </template>
-      <span>{{ userGameStore.user.health }}</span>
+      <span>{{ user.health }}</span>
     </ListItem>
     <ListItem>
       <template #title> Mana: </template>
-      <span>{{ userGameStore.user.mana }}</span>
+      <span>{{ user.mana }}</span>
     </ListItem>
     <ListItem>
       <template #title> Energy: </template>
-      <span>{{ userGameStore.user.energy }}</span>
+      <span>{{ user.energy }}</span>
     </ListItem>
     <ListItem>
       <template #title> Armor: </template>
-      <span>{{ userGameStore.user.armor }}</span>
+      <span>{{ user.armor }}</span>
     </ListItem>
     <ListTitle>Character Points</ListTitle>
     <ListItem>
       <template #title> Attack: </template>
-      <span>{{ userGameStore.user.charPoint.attack }}</span>
+      <span>{{ user.charPoint.attack }}</span>
     </ListItem>
     <ListItem>
       <template #title> Defend: </template>
-      <span>{{ userGameStore.user.charPoint.defend }}</span>
+      <span>{{ user.charPoint.defend }}</span>
     </ListItem>
     <ListTitle>Timers</ListTitle>
-    <ListItem convertable @convert="convert()">
-      <template #title> Health: </template>
-      <span>{{ userGameStore.user.timer.getHealth.toString() }}</span>
-    </ListItem>
-    <ListItem convertable @convert="convert()">
-      <template #title> Mana: </template>
-      <span>{{ userGameStore.user.timer.getMana.toString() }}</span>
-    </ListItem>
-    <ListItem convertable @convert="convert()">
-      <template #title> Energy: </template>
-      <span>{{ userGameStore.user.timer.getEnergy.toString() }}</span>
-    </ListItem>
-    <ListItem convertable @convert="convert()">
-      <template #title> Revive: </template>
-      <span>{{ userGameStore.user.timer.revive.toString() }}</span>
-    </ListItem>
-    <ListItem convertable @convert="convert()">
-      <template #title> Teleport: </template>
-      <span>{{ userGameStore.user.timer.teleport.toString() }}</span>
-    </ListItem>
-    <ListItem convertable @convert="convert()">
-      <template #title> Town TP: </template>
-      <span>{{ userGameStore.user.timer.teleportToTown.toString() }}</span>
-    </ListItem>
-    <ListItem convertable @convert="convert()">
-      <template #title> Prepare: </template>
-      <span>{{ userGameStore.user.timer.prepareToAttack.toString() }}</span>
+    <ListItem
+      v-for="(item, index) in timers"
+      :key="index"
+      convertable
+      @convert="(isConvert) => convert(isConvert, item as any)"
+    >
+      <template #title> {{ toCapitalizedWords(item) }}: </template>
+      <span>{{
+        timer[item].toString() === '0' ? 'Available!' : timer[item].toString()
+      }}</span>
     </ListItem>
   </div>
 </template>
 
 <script setup lang="ts">
+import { duration } from 'moment'
+import { ethers } from 'ethers'
 import ListTitle from '~/components/sidebar-items/ListTitle.vue'
 import ListItem from '~/components/sidebar-items/ListItem.vue'
-
-const convertTime = ref(24)
+import { IKillThemAll } from '~/types/typechain/contracts/game/KillThemAll'
 
 const userGameStore = useUserGameStore()
+const userWalletStore = useUserWalletStore()
+const { user } = storeToRefs(userGameStore)
+const timer = reactive<any>({ ...user.value.timer })
+const timers = Object.keys(user.value.timer).filter((item: any) => isNaN(item))
+const stringName = ref(ethers.utils.parseBytes32String(user.value.name as any))
+const nameError = ref('')
+const reffererAddress = user.value.referrer as string
 
-const userName = computed(() =>
-  userGameStore.user.name ===
-  '0x0000000000000000000000000000000000000000000000000000000000000000'
-    ? 'user'
-    : userGameStore.user.name
-)
-
-console.log(userGameStore.user)
-
-const general = reactive({
-  name: 'Hadalobo',
-  coordinate: '(0,1)',
-  level: 4,
-  exp: 5,
-  address: '0xB55F8FC6de35c643a2Ed462d3316706A4159D41D',
-})
-const stats = reactive({
-  health: 24,
-  mana: 54,
-  energy: 17,
-  armor: 18,
-})
-const timer = reactive({
-  health: 23237438573957,
-  mana: 23237438573957,
-  energy: 23237438573957,
-  revive: 23237438573957,
-  teleport: 23237438573957,
-  townTP: 23237438573957,
-  prepare: 23237438573957,
-})
-const characterPoints = reactive({
-  attack: 3,
-  defind: 1,
-})
-
-const reffererAddress = userGameStore.user.referrer as string
 const referrer = computed(
   () =>
     reffererAddress.substring(0, 5) +
@@ -136,7 +100,39 @@ const referrer = computed(
     reffererAddress.substring(reffererAddress.length - 5)
 )
 
-const convert = () => {
-  timer.health = convertTime.value
+const maxString = () => {
+  try {
+    ethers.utils.formatBytes32String(stringName.value)
+    nameError.value = ''
+  } catch (error) {
+    nameError.value = 'Name must be less than 32 bytes'
+  }
+}
+
+const toCapitalizedWords = (name: string) => {
+  const words = name.match(/[A-Za-z][a-z]*/g) || []
+
+  return words.map(capitalize).join(' ')
+}
+
+const capitalize = (word: string) =>
+  word.charAt(0).toUpperCase() + word.substring(1)
+
+const convert = (
+  isConvert: boolean,
+  propertyName: keyof IKillThemAll.UserTimerStruct
+) => {
+  if (isConvert) {
+    timer[propertyName] =
+      timer[propertyName] - userWalletStore.currentBlockNumber > 0
+        ? duration(
+            (timer[propertyName] - userWalletStore.currentBlockNumber) *
+              12 *
+              1000
+          ).humanize()
+        : 0
+  } else {
+    timer[propertyName] = user.value.timer[propertyName].toString()
+  }
 }
 </script>
