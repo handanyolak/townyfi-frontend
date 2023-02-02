5e7abbc8b31a782add2ffe212353ad1ffd807fb6
handanyolak <handanyolak@gmail.com>
2023-02-02 16:04:19 +0300

    Create Actions component

---
+++ components/Actions.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <ListTitle>Teleport</ListTitle>
+    <div class="grid grid-cols-2">
+      <ListItem editable>
+        <template #title> X: </template>
+        <template #input>
+          <input v-model="coordinateX" type="text" />
+        </template>
+        <span>{{ coordinateX }}</span>
+      </ListItem>
+      <ListItem editable>
+        <template #title> Y: </template>
+        <template #input>
+          <input v-model="coordinateY" type="text" />
+        </template>
+        <span>{{ coordinateY }}</span>
+      </ListItem>
+    </div>
+    <div class="flex justify-center">
+      <TownyButton @click="teleport()">Teleport</TownyButton>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ListTitle from '~/components/sidebar-items/ListTitle.vue'
+import ListItem from '~/components/sidebar-items/ListItem.vue'
+import { useUserGameStore } from '~/stores/userGame'
+
+const kta = useKta()
+const userGameStore = useUserGameStore()
+const { user } = storeToRefs(userGameStore)
+
+const coordinateX = user.value?.coordinate._x
+const coordinateY = user.value?.coordinate._y
+
+const teleport = async () => {
+  await kta.teleport({
+    _x: coordinateX,
+    _y: coordinateY,
+  })
+}
+</script>
