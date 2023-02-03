054c7413b94968723174218427bf73ade1d8874b
handanyolak <handanyolak@gmail.com>
2023-02-03 21:50:44 +0300

    Use JSON instead of direct to i18n

--- composables/useLang.ts
+++ composables/useLang.ts
@@ -1,17 +1,15 @@
-import enJson from '~/locales/en.json'
+import en from '~/locales/en.json'
+import tr from '~/locales/tr.json'
+import de from '~/locales/de.json'
 
 export const useLang = (word: string) => {
   const userOptionsStore = useUserOptionsStore()
   const { language } = userOptionsStore
 
   const words = {
-    en: enJson,
-    tr: {
-      disconnect_wallet: 'Cüzdan Bağlantısını Kes',
-    },
-    gr: {
-      disconnect_wallet: 'Ich lie',
-    },
+    en,
+    tr,
+    de,
   }
 
   return words[language][word]
