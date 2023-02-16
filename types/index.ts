cc534ae315032a815f3acfc6fe4d85350917f87f
handanyolak <handanyolak@gmail.com>
2023-02-16 19:28:19 +0300

    Add yup interface

--- types/index.ts
+++ types/index.ts
@@ -1,4 +1,12 @@
+import * as yup from 'yup'
+
 export * from './typechain'
 export * from './components/CastleBox'
 export * from './components/SidebarTab'
 export * from './TownyToast'
+
+declare module 'yup' {
+  interface StringSchema {
+    bytes32(message?: yup.Message<any>): this
+  }
+}
