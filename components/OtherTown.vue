ea0e6fe701de21f4511fc99efff2343bbadd2166
handanyolak <handanyolak@gmail.com>
2023-02-22 21:09:49 +0300

    Create OtherTown.vue

---
+++ components/OtherTown.vue
@@ -0,0 +1,159 @@
+<template>
+  <div>
+    <ListTitle>General</ListTitle>
+    <ListItem tooltip>
+      <template #title> Name: </template>
+      <span>{{ ethers.utils.parseBytes32String(townInfo.name as any) }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem tooltip>
+      <template #title> Coordinate </template>
+      <span>({{ townInfo.coordinate._x.toString() }}</span>
+      <span>,</span>
+      <span>{{ townInfo.coordinate._y.toString() }})</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem tooltip>
+      <template #title> Level: </template>
+      <span>{{ townInfo.levelId }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem tooltip>
+      <template #title> Exp: </template>
+      <span>{{ townInfo.exp }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem copiable tooltip :copy-value="townInfo.leader">
+      <template #title> Leader: </template>
+      <span>{{ leader }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem tooltip>
+      <template #title> Status: </template>
+      <span>{{ TownStatus[townInfo.status] }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListTitle>Citizens</ListTitle>
+    <ScrollableList
+      :items="citizenAddresses"
+      :copy-value="addresses"
+      copiable
+    />
+    <ListTitle>Timers</ListTitle>
+    <ListItem tooltip>
+      <template #title> Protection: </template>
+      <span>{{ townInfo.protectionAt.toString() }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListTitle>War</ListTitle>
+    <ListItem tooltip>
+      <template #title> Attacker: </template>
+      <span>{{ attacker }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem tooltip>
+      <template #title> Defender: </template>
+      <span>{{ defender }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem tooltip>
+      <template #title> Attackable: </template>
+      <span>{{ attackable }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+    <ListItem tooltip>
+      <template #title> Expired: </template>
+      <span>{{ expired }}</span>
+      <template #tooltip>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
+      </template>
+    </ListItem>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ethers } from 'ethers'
+import * as yup from 'yup'
+import ListTitle from '~/components/sidebar-items/ListTitle.vue'
+import ListItem from '~/components/sidebar-items/ListItem.vue'
+import ScrollableList from '~/components/sidebar-items/ScrollableList.vue'
+import { TownStatus } from '~/constants'
+
+interface OtherTownProps {
+  id: string
+}
+const props = defineProps<OtherTownProps>()
+
+const nameRules = yup.string().bytes32()
+const kta = useKta()
+const townInfo = {
+  ...(await kta.townById(props.id)),
+}
+const townName = ref(ethers.utils.parseBytes32String(townInfo.name))
+
+const addresses = await kta.getCitizensByTownId(props.id)
+
+const attacker = ref(1)
+const defender = ref(2)
+const attackable = ref(3743879)
+const expired = ref(3743879)
+
+const leader = computed(() => middleCropping(townInfo.leader))
+
+const citizenAddresses = computed(() =>
+  addresses.map((address) => middleCropping(address))
+)
+</script>
