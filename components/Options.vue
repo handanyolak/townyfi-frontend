a6d1b4e209b650238d18295cdf2e1a849fb719fc
handanyolak <handanyolak@gmail.com>
2023-03-01 09:06:51 +0300

    Create Options component

--- components/Options.vue
+++ components/Options.vue
@@ -1,5 +1,56 @@
 <template>
-  <div>Options</div>
+  <div>
+    <ListTitle>General</ListTitle>
+    <ListItem>
+      <template #title> Coordinate: </template>
+      <LanguageDropdown />
+    </ListItem>
+    <ListItem>
+      <template #title> Theme: </template>
+      <Switch @toggled="toggleTheme()">
+        <template #item-1>Light</template>
+        <template #item-2>Dark</template>
+      </Switch>
+    </ListItem>
+    <ListItem>
+      <template #title> Sound: </template>
+      <Switch @toggled="appOptionStore.toggleAudio()">
+        <template #item-1>Yes</template>
+        <template #item-2>No</template>
+      </Switch>
+    </ListItem>
+    <ListItem>
+      <template #title> Music: </template>
+      <Switch @toggled="appOptionStore.toggleMusic()">
+        <template #item-1>Yes</template>
+        <template #item-2>No</template>
+      </Switch>
+    </ListItem>
+    <ListTitle>Interface</ListTitle>
+    <ListItem>
+      <template #title> Near Level: </template>
+      <Switch>
+        <template #item-1>1</template>
+        <template #item-2>2</template>
+      </Switch>
+    </ListItem>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useDark, useToggle } from '@vueuse/core'
+import ListTitle from '~/components/sidebar-items/ListTitle.vue'
+import ListItem from '~/components/sidebar-items/ListItem.vue'
+import LanguageDropdown from '~/components/LanguageDropdown.vue'
+import Switch from '~/components/Switch.vue'
+
+const appOptionStore = useAppOptionsStore()
+
+const isDark = useDark({
+  storageKey: 'theme',
+  valueDark: 'dark',
+  valueLight: 'light',
+})
+
+const toggleTheme = useToggle(isDark)
+</script>
