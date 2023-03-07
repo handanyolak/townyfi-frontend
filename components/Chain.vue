0f42d1e9ab0016d6b7f1f3255bd57e730732455b
handanyolak <handanyolak@gmail.com>
2023-03-07 10:56:00 +0300

    Create Chain component

---
+++ components/Chain.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <ListTitle>General</ListTitle>
+    <ListItem>
+      <template #title> Name: </template>
+      <span>xxx network</span>
+    </ListItem>
+    <ListItem>
+      <template #title> Chain Id: </template>
+      <span>1111</span>
+    </ListItem>
+    <ListItem>
+      <template #title> Symbol: </template>
+      <span>ETH</span>
+    </ListItem>
+    <ListTitle>Explorer URLs</ListTitle>
+    <ScrollableList
+      :items="[
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+      ]"
+      :copy-value="[
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+        'https://example1.com',
+      ]"
+      copiable
+    />
+    <ListTitle>Miscellaneous</ListTitle>
+    <ListItem>
+      <template #title> Current Block Number: </template>
+      <span>11109491</span>
+    </ListItem>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ListTitle from '~/components/sidebar-items/ListTitle.vue'
+import ListItem from '~/components/sidebar-items/ListItem.vue'
+import ScrollableList from '~/components/sidebar-items/ScrollableList.vue'
+</script>
