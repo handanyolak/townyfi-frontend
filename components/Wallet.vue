b7f6ebbf7697cd648d82dbfddacb106e8128f101
handanyolak <handanyolak@gmail.com>
2023-03-09 16:43:01 +0300

    Create Wallet component

---
+++ components/Wallet.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <ListTitle>General</ListTitle>
+    <ListItem>
+      <template #title> Address: </template>
+      <span>0Xfslkflsf</span>
+    </ListItem>
+    <ListItem>
+      <template #title> Balance: </template>
+      <span>1111</span>
+    </ListItem>
+    <ListTitle>Miscellaneous</ListTitle>
+    <ListItem>
+      <template #title> Game Token Balance: </template>
+      <span>11109491</span>
+    </ListItem>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ListTitle from '~/components/sidebar-items/ListTitle.vue'
+import ListItem from '~/components/sidebar-items/ListItem.vue'
+</script>
