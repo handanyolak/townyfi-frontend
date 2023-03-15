47beb36826c0654b25fd071921770f9fb963c968
handanyolak <handanyolak@gmail.com>
2023-03-15 21:36:44 +0300

    Fix styles

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -1,85 +1,87 @@
 <template>
-  <div class="container">
-    <div class="flex justify-between py-5">
-      <span
-        class="flex select-none items-center bg-gradient-to-r from-towni-brown-dark-400 via-towni-brown-dark-400 to-towni-brown-dark-200 bg-clip-text text-5xl font-extrabold text-transparent"
-        style="font-family: Pirata One, sans-serif"
-      >
-        <img class="h-7 w-7" src="@/assets/img/paper-document.svg" />
-        TownyFi
-      </span>
-      <div class="flex items-center space-x-2">
-        <div v-if="hasMetamask" class="flex justify-between py-5">
-          <div v-if="onValidNetwork">
-            <div v-if="isConnected" class="space-x-1">
+  <div class="absolute top-0 w-full">
+    <div class="container">
+      <div class="mb-10 flex justify-between py-5">
+        <span
+          class="flex select-none items-center bg-gradient-to-r from-towni-brown-dark-400 via-towni-brown-dark-400 to-towni-brown-dark-200 bg-clip-text text-5xl font-extrabold text-transparent"
+          style="font-family: Pirata One, sans-serif"
+        >
+          <img class="h-7 w-7" src="@/assets/img/paper-document.svg" />
+          TownyFi
+        </span>
+        <div class="flex items-center space-x-2">
+          <div v-if="hasMetamask" class="flex justify-between py-5">
+            <div v-if="onValidNetwork">
+              <div v-if="isConnected" class="space-x-1">
+                <TownyButton
+                  v-if="!isRegistered"
+                  fill
+                  hover-effect
+                  @click.native="toggleModal(true)"
+                >
+                  Register
+                </TownyButton>
+                <TownyButton fill hover-effect @click.native="disconnectWeb3()">
+                  {{ $t('disconnect_wallet') }}
+                </TownyButton>
+              </div>
               <TownyButton
-                v-if="!isRegistered"
-                fill
+                v-else
+                border
                 hover-effect
-                @click.native="toggleModal(true)"
+                @click.native="connectWeb3()"
               >
-                Register
-              </TownyButton>
-              <TownyButton fill hover-effect @click.native="disconnectWeb3()">
-                {{ $t('disconnect_wallet') }}
+                Connect Wallet
               </TownyButton>
             </div>
             <TownyButton
               v-else
+              target="_blank"
               border
               hover-effect
-              @click.native="connectWeb3()"
+              @click.native="switchNetwork()"
             >
-              Connect Wallet
+              Switch Network
             </TownyButton>
           </div>
           <TownyButton
             v-else
+            href="https://metamask.io/download/"
             target="_blank"
             border
             hover-effect
-            @click.native="switchNetwork()"
           >
-            Switch Network
+            Install Metamask
           </TownyButton>
+          <Dropdown
+            :select="language"
+            :dropdown-items="languages"
+            :icon-names="languages"
+            @selected="(item) => selected(item)"
+          />
+          <img
+            :src="themeIcon"
+            class="h-5 w-5 cursor-pointer"
+            @click="toggleTheme()"
+          />
+          <img
+            :src="audioIcon"
+            class="h-7 w-7 cursor-pointer"
+            @click="appOptionStore.toggleAudio()"
+          />
+          <img
+            v-if="audio"
+            :src="musicIcon"
+            class="h-5 w-5 cursor-pointer"
+            @click="appOptionStore.toggleMusic()"
+          />
         </div>
-        <TownyButton
-          v-else
-          href="https://metamask.io/download/"
-          target="_blank"
-          border
-          hover-effect
-        >
-          Install Metamask
-        </TownyButton>
-        <Dropdown
-          :select="language"
-          :dropdown-items="languages"
-          :icon-names="languages"
-          @selected="(item) => selected(item)"
-        />
-        <img
-          :src="themeIcon"
-          class="h-5 w-5 cursor-pointer"
-          @click="toggleTheme()"
-        />
-        <img
-          :src="audioIcon"
-          class="h-7 w-7 cursor-pointer"
-          @click="appOptionStore.toggleAudio()"
-        />
-        <img
-          v-if="audio"
-          :src="musicIcon"
-          class="h-5 w-5 cursor-pointer"
-          @click="appOptionStore.toggleMusic()"
-        />
       </div>
-    </div>
 
-    <InformationModal v-if="showModal" @modalClosed="toggleModal(false)">
-      <Register @registerClosed="toggleModal(false)" />
-    </InformationModal>
+      <InformationModal v-if="showModal" @modalClosed="toggleModal(false)">
+        <Register @registerClosed="toggleModal(false)" />
+      </InformationModal>
+    </div>
   </div>
 </template>
 
