90b416693ad38b0af5d39f5b4adf6a96a3859980
handanyolak <handanyolak@gmail.com>
2023-04-13 07:23:03 +0300

    feature(MoveArrow): Convert Arrow into a component

---
+++ components/MoveArrow.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="arrow left-[50%] w-10"></div>
+</template>
+
+<style scoped>
+.arrow {
+  @apply absolute h-2 origin-left transform  cursor-pointer bg-towni-brown-dark-200 opacity-50 transition-all duration-300 ease-in-out before:absolute before:-right-3 before:-top-2 before:bottom-0 before:h-0 before:w-0 before:border-t-12 before:border-b-12 before:border-l-18 before:border-towni-brown-dark-200 before:border-b-transparent before:border-t-transparent active:opacity-95;
+}
+</style>
