c66aba3b1bb3cdd701159078eb4bcddb9511df01
handanyolak <handanyolak@gmail.com>
2023-04-14 07:27:09 +0300

    feature(MapNavigation): Create Map navigation component

---
+++ components/MapNavigation.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <Transition name="map-navigation">
+      <div
+        :style="`height: ${height}px`"
+        :class="[
+          'absolute top-0 -right-52  transition-all duration-300 ease-in-out',
+          isMapNavigation ? '' : '-translate-x-52',
+        ]"
+      >
+        <img
+          :class="[
+            'absolute top-0 -right-4 h-3 w-3 transform cursor-pointer transition-transform duration-200 ease-in-out',
+            isMapNavigation ? 'rotate-180' : '',
+          ]"
+          src="@/assets/img/multiple-arrow.svg"
+          @click="isMapNavigation = !isMapNavigation"
+        />
+        <div class="flex h-full w-52 flex-col justify-between bg-[#391f05] p-2">
+          <div class="move">
+            <ListTitle class="shadow-md shadow-towni-brown-dark-400"
+              ><span class="text-sm">Move</span></ListTitle
+            >
+            <div class="relative mt-6 flex items-center justify-center p-10">
+              <MoveArrow
+                @click="navigateByArrow(Direction.Down)"
+                class="rotate-90"
+              />
+              <MoveArrow
+                @click="navigateByArrow(Direction.Up)"
+                class="-rotate-90"
+              />
+              <MoveArrow
+                @click="navigateByArrow(Direction.Right)"
+                class="rotate-0"
+              />
+              <MoveArrow
+                @click="navigateByArrow(Direction.Left)"
+                class="rotate-180"
+              />
+              <MoveArrow class="rotate-45" />
+              <MoveArrow class="-rotate-45" />
+              <MoveArrow class="rotate-[135deg]" />
+              <MoveArrow class="-rotate-[135deg]" />
+            </div>
+          </div>
+          <div class="zoom">
+            <ListTitle class="text-sm shadow-md shadow-towni-brown-dark-400"
+              >Zoom in / Zoom out</ListTitle
+            >
+            <div class="flex justify-around">
+              <button
+                @click="setNearLevelByCalculatingCoordinates(nearLevel - 1)"
+              >
+                <img
+                  class="h-12 w-12"
+                  src="@/assets/img/zoom-in.svg"
+                  alt="zoom-in"
+                />
+              </button>
+              <button
+                @click="setNearLevelByCalculatingCoordinates(nearLevel + 1)"
+              >
+                <img
+                  class="h-12 w-12"
+                  src="@/assets/img/zoom-out.svg"
+                  alt="zoom-out"
+                />
+              </button>
+            </div>
+          </div>
+          <div class="teleport">
+            <ListTitle class="text-sm shadow-md shadow-towni-brown-dark-400"
+              >Teleport</ListTitle
+            >
+            <div class="flex items-center rounded-md p-1">
+              <div class="flex flex-col">
+                <ListItem class="shadow-none" input>
+                  <template #title> X: </template>
+                  <template #item>
+                    <input v-model="coordinateX" type="text" />
+                  </template>
+                  <span>{{ coordinateX }}</span>
+                </ListItem>
+                <ListItem class="shadow-none" input>
+                  <template #title> Y: </template>
+                  <template #item>
+                    <input v-model="coordinateY" type="text" />
+                  </template>
+                  <span>{{ coordinateY }}</span>
+                </ListItem>
+                <TownyButton
+                  @click="teleport()"
+                  class="w-full rounded-md bg-towni-brown-dark-400 text-towni-brown-light-400"
+                >
+                  Go!
+                </TownyButton>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Transition>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Direction } from '~/enums'
+import ListItem from '~/components/sidebar-items/ListItem.vue'
+import ListTitle from '~/components/sidebar-items/ListTitle.vue'
+import MoveArrow from '~/components/MoveArrow.vue'
+
+//--------[ Props & Emits ]--------//
+defineProps({
+  height: {
+    default: '200px',
+  },
+})
+
+//--------[ Stores ]--------//
+const appOptionsStore = useAppOptionsStore()
+const userGameStore = useUserGameStore()
+const { setUserCoordinate, setNearLevelByCalculatingCoordinates } =
+  userGameStore
+
+const { originCoordinate } = storeToRefs(appOptionsStore)
+const { nearLevel } = storeToRefs(userGameStore)
+
+//--------[ Data ]--------//
+const coordinateX = ref('')
+const coordinateY = ref('')
+const isMove = ref(-1)
+const isMapNavigation = ref(false)
+
+//--------[ Methods ]--------//
+const teleport = () => {
+  setUserCoordinate({
+    _x: BigInt(coordinateX.value),
+    _y: BigInt(coordinateY.value),
+  })
+}
+
+const navigateByArrow = (direction: Direction) => {
+  console.log(direction)
+  // TODO:Ethers try this
+  const _originCoordinate = { ...originCoordinate.value }
+
+  const navigateValue = BigInt(1)
+  if (direction === Direction.Up) {
+    _originCoordinate._y = _originCoordinate._y + navigateValue
+  } else if (direction === Direction.Right) {
+    _originCoordinate._x = _originCoordinate._x + navigateValue
+  } else if (direction === Direction.Down) {
+    _originCoordinate._y = _originCoordinate._y - navigateValue
+  } else {
+    _originCoordinate._x = _originCoordinate._x - navigateValue
+  }
+
+  setUserCoordinate(_originCoordinate)
+}
+
+// const userMove = async (direction: Direction) => {
+//   isMove.value = direction
+//   try {
+//     await kta.connect(connectionStore.signer).move(direction)
+//     isMove.value = -1
+//   } catch (error) {
+//     isMove.value = -1
+//     console.log(error)
+//   }
+// }
+</script>
+
+<style>
+.map-navigation-enter-active,
+.map-navigation-leave-active {
+  transition: transform 0.8s ease;
+}
+
+.map-navigation-enter-from,
+.map-navigation-leave-to {
+  transform: translateX(-100%);
+  transition: all 0.3s ease-out;
+}
+</style>
