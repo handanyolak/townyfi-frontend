62883e477fef6365da9ec18f3a3dfa52c825becd
handanyolak <handanyolak@gmail.com>
2023-04-15 07:47:24 +0300

    refactor(TheLoading): Add transition

--- components/TheLoading.vue
+++ components/TheLoading.vue
@@ -1,33 +1,67 @@
 <template>
-  <div
-    class="modal-backdrop fixed inset-0 z-50 h-full w-full bg-towni-brown-light-100 dark:bg-night-blue"
-  >
-    <div class="flex min-h-screen items-center justify-center">
-      <div class="loader flex space-x-3 rounded-full px-20 py-10">
-        <div class="h-10 w-10 animate-bounce rounded-full">
-          <img src="~/assets/img/swords.svg" alt="swords" />
-        </div>
-        <div class="h-10 w-10 animate-bounce rounded-full">
-          <img
-            class="block dark:hidden"
-            src="~/assets/img/knight.svg"
-            alt="knight"
-          />
-          <img
-            class="hidden dark:block"
-            src="~/assets/img/soldier.svg"
-            alt="soldier"
-          />
-        </div>
-        <div class="h-10 w-10 animate-bounce rounded-full">
-          <img src="~/assets/img/swords.svg" alt="swords" />
+  <Transition name="loading-slide">
+    <div
+      class="modal-backdrop fixed inset-0 z-50 h-full w-full bg-towni-brown-light-100 dark:bg-night-blue"
+    >
+      <div class="flex min-h-screen items-center justify-center">
+        <div class="loader flex space-x-3 rounded-full px-20 py-10">
+          <div class="h-10 w-10 animate-bounce rounded-full">
+            <img src="~/assets/img/swords.svg" alt="swords" />
+          </div>
+          <div class="h-10 w-10 animate-bounce rounded-full">
+            <img
+              class="block dark:hidden"
+              src="~/assets/img/knight.svg"
+              alt="knight"
+            />
+            <img
+              class="hidden dark:block"
+              src="~/assets/img/soldier.svg"
+              alt="soldier"
+            />
+          </div>
+          <div class="h-10 w-10 animate-bounce rounded-full">
+            <img src="~/assets/img/swords.svg" alt="swords" />
+          </div>
         </div>
       </div>
     </div>
-  </div>
+  </Transition>
 </template>
 
 <style scoped>
+.loading-slide-enter {
+  opacity: 0;
+}
+
+.loading-slide-enter-active {
+  animation: slide-in 1s ease-in forwards;
+  transition: opacity 0.5s;
+}
+
+.loading-slide-leave-active {
+  animation: slide-out 1s ease-out forwards;
+  opacity: 0;
+  transition: opacity 1s;
+}
+
+@keyframes slide-in {
+  from {
+    transform: translateY(20px);
+  }
+  to {
+    transform: translateY(0);
+  }
+}
+
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+  }
+  to {
+    transform: translateY(20px);
+  }
+}
 .loader div {
   animation-duration: 0.5s;
 }
