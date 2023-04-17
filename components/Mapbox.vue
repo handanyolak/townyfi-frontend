9f9dcc4a392c476080d26da410df28c908d3c4c7
handanyolak <handanyolak@gmail.com>
2023-04-17 03:48:27 +0300

    refactor(Mapbox):Fix Mapbox items

--- components/Mapbox.vue
+++ components/Mapbox.vue
@@ -1,51 +1,49 @@
 <template>
   <div
-    style="box-shadow: 0px 1px 0px 1px rgba(255, 205, 116, 0.4)"
-    class="group relative border-towni-brown-light-500 dark:border-[#02024c]"
+    class="group relative shadow-towni-400"
     :class="isCoordinateOfUser ? 'bg-gray-200 dark:bg-gray-900' : ''"
   >
-    <div class="absolute -top-7 flex w-full justify-between">
-      <div
-        class="invisible z-50 flex items-center justify-center group-hover:visible"
-      >
-        <div class="bg-towni-brown-light-100 font-bold dark:bg-[#040413]">
-          <div
-            class="bg-gradient-to-b from-[#bf841a] via-[#bf841a] to-[#ffcd74] bg-clip-text text-sm text-transparent"
-          >
-            <span>({{ item._x.toString() }}</span>
-            <span>,</span>
-            <span>{{ item._y.toString() }})</span>
-          </div>
-        </div>
-      </div>
-
-      <div
-        class="rounded-shadow flex h-20 w-20 items-center justify-center rounded-full"
-      >
-        <svg class="mr-3 h-5 w-5 animate-spin" viewBox="0 0 24 24"></svg>
-        <span
-          :class="{
-            'animate-ping bg-gray-500': getUserCountByCoordinate.get(getMapKey) === undefined,
-            'bg-zinc-50': getUserCountByCoordinate.get(getMapKey) === 0, // TODO: remove this
-            'bg-green-500': getUserCountByCoordinate.get(getMapKey) === 1, // TODO: use <=
-            'bg-yellow-500':
-            getUserCountByCoordinate.get(getMapKey)!! > 2 && getUserCountByCoordinate.get(getMapKey)!! < 3,
-            'animate-ping bg-red-500': getUserCountByCoordinate.get(getMapKey)!! >= 4,
-          }"
-          class="inline-block h-1 w-1 rounded-full"
-        >
-        </span>
-      </div>
-    </div>
-    <div
-      class="relative flex cursor-pointer flex-col items-center justify-center"
-      @click="toggleModal()"
-    >
+    <div class="relative flex flex-col items-center justify-center">
       <img
         v-if="getHasTownByCoordinate.get(getMapKey)"
         src="@/assets/img/town.png"
       />
       <img v-else src="@/assets/img/skeleton-castle.png" class="invisible" />
+    </div>
+    <div>
+      <div
+        class="duration-400 absolute top-0 left-0 z-50 flex w-16 items-center justify-between bg-towni-brown-dark-600 bg-opacity-60 px-1 py-1 opacity-0 transition-opacity group-hover:opacity-100"
+      >
+        <img
+          @click="toggleModal()"
+          class="h-4 w-4 cursor-pointer"
+          src="@/assets/img/information.svg"
+        />
+        <div class="text-xs text-towni-brown-light-200">
+          <span>({{ item._x.toString() }}</span>
+          <span>,</span>
+          <span>{{ item._y.toString() }})</span>
+        </div>
+      </div>
+      <div class="absolute top-1 right-1">
+        <span class="relative flex">
+          <span
+            :class="[
+              'absolute inline-flex h-full w-full animate-ping rounded-full opacity-75',
+              pulse,
+              nearLevel <= 5 ? 'h-2 w-2' : 'h-1 w-1',
+            ]"
+          />
+          <span
+            :class="[
+              'relative inline-flex h-1 w-1 rounded-full',
+              pulse,
+              nearLevel <= 5 ? 'h-2 w-2' : 'h-1 w-1',
+            ]"
+            class=""
+          />
+        </span>
+      </div>
     </div>
   </div>
 </template>
@@ -54,26 +52,22 @@
 import { CoordinateItem } from '~/types'
 import { useUserGameStore } from '~/stores/userGame'
 
-/**
- * Props & Emits
- */
+//--------[ Props & Emits ]--------//
 interface MapboxProps {
   item: CoordinateItem
 }
 
+const pulseColor = ref('')
+
 const props = defineProps<MapboxProps>()
 const emit = defineEmits(['modalOpened'])
 
-/**
- * Stores
- */
+//--------[ Stores ]--------//
 const userGameStore = useUserGameStore()
-const { user, getUserCountByCoordinate, getHasTownByCoordinate } =
+const { user, getUserCountByCoordinate, getHasTownByCoordinate, nearLevel } =
   storeToRefs(userGameStore)
 
-/**
- * Computeds
- */
+//--------[ Computeds ]--------//
 const isCoordinateOfUser = computed(
   () =>
     props.item._x.toString() === user.value.coordinate._x.toString() &&
@@ -84,6 +78,25 @@ const getMapKey = computed(
   () => `${props.item._x.toString()},${props.item._y.toString()}`
 )
 
+const pulse = computed(() => {
+  if (getUserCountByCoordinate.value.get(getMapKey.value) === undefined) {
+    pulseColor.value = 'bg-gray-500'
+  } else if (getUserCountByCoordinate.value.get(getMapKey.value) === 0) {
+    pulseColor.value = 'bg-zinc-50'
+  } else if (getUserCountByCoordinate.value.get(getMapKey.value) === 1) {
+    pulseColor.value = 'bg-green-500'
+  } else if (
+    getUserCountByCoordinate.value.get(getMapKey.value)!! >= 2 &&
+    getUserCountByCoordinate.value.get(getMapKey.value)!! <= 3
+  ) {
+    pulseColor.value = 'bg-yellow-600'
+  } else if (getUserCountByCoordinate.value.get(getMapKey.value)!! >= 4) {
+    pulseColor.value = 'bg-red-500'
+  }
+  return pulseColor.value
+})
+
+//--------[ Methods ]--------//
 /**
  * Bu fonksiyon Parent'a model acildi bilgisini verecek event'i yayinlayan fonksiyondur.
  * @Component Mapbox
@@ -92,23 +105,3 @@ const toggleModal = () => {
   emit('modalOpened', props.item)
 }
 </script>
-
-<style>
-/* .deneme3 {
-  background-image: url('@/assets/img/map.png');
-  background-position: 100% 89%;
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  border: 1px solid;
-}
-.deneme4 {
-  background-image: url('@/assets/img/map.png');
-
-  background-size: 100%;
-  background-repeat: no-repeat;
-  background-attachment: fixed;
-  background-position: 100% 89%;
-  border: 1px solid;
-} */
-</style>
