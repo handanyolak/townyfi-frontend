a33857709df85a49aecefea2191c24aa217a738a
handanyolak <handanyolak@gmail.com>
2023-05-10 00:15:54 +0300

    feat(HasNotTown): Create Has not town component and Add CreateTown and FindTown component

---
+++ components/HasNotTown.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <CreateTown />
+    <p class="my-5 text-center font-bold">OR</p>
+    <FindTown />
+  </div>
+</template>
+
+<script setup lang="ts">
+import CreateTown from '~/components/CreateTown.vue'
+import FindTown from '~/components/FindTown.vue'
+
+//--------[ Stores ]--------//
+</script>
