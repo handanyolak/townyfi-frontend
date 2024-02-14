0812591f6160e8b7380128cd5e54d0689777573e
handanyolak <handanyolak@gmail.com>
2024-02-14 16:42:16 +0300

    refactor(Interactions): Refactor interactions component layout and add tooltips

--- components/Interactions.vue
+++ components/Interactions.vue
@@ -1,122 +1,96 @@
 <template>
   <div>
-    <Accordion>
+    <Accordion tooltip>
       <template #title>
-        <div class="group flex">
-          <Tooltip class="self-center">
-            <template #tooltip-image>
-              <img
-                class="mr-1 h-3 w-3 cursor-pointer opacity-0 transition-opacity duration-500 ease-out group-hover:opacity-100"
-                src="@/assets/img/information.svg"
-              />
+        <span class="m-auto"> Teleport </span>
+      </template>
+      <template #tooltip>
+        <span> Lorem ipsum, dolor sit amet consectetur adipisicing. </span>
+      </template>
+
+      <template #content>
+        <div class="flex">
+          <ListItem input>
+            <template #title> X: </template>
+            <template #item>
+              <input v-model="coordinateX" type="text" />
             </template>
-            <span
-              >Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, deserunt!</span
-            >
-          </Tooltip>
-          <span class="m-auto">Teleport</span>
+            <span>{{ coordinateX }}</span>
+          </ListItem>
+          <ListItem input>
+            <template #title> Y: </template>
+            <template #item>
+              <input v-model="coordinateY" type="text" />
+            </template>
+            <span>{{ coordinateY }}</span>
+          </ListItem>
+          <img
+            class="cursor-pointer"
+            src="@/assets/img/check.svg"
+            alt="teleport"
+            @click="onClickTeleport()"
+          />
         </div>
       </template>
-      <div class="flex">
-        <ListItem input>
-          <template #title> X: </template>
-          <template #item>
-            <input v-model="coordinateX" type="text" />
-          </template>
-          <span>{{ coordinateX }}</span>
-        </ListItem>
-        <ListItem input>
-          <template #title> Y: </template>
-          <template #item>
-            <input v-model="coordinateY" type="text" />
-          </template>
-          <span>{{ coordinateY }}</span>
-        </ListItem>
-        <img
-          class="cursor-pointer"
-          src="@/assets/img/check.svg"
-          alt="teleport"
-          @click="onClickTeleport()"
-        />
-      </div>
     </Accordion>
-    <Accordion>
+    <Accordion tooltip>
       <template #title>
-        <div class="group flex">
-          <Tooltip class="self-center">
-            <template #tooltip-image>
-              <img
-                class="mr-1 h-3 w-3 cursor-pointer opacity-0 transition-opacity duration-500 ease-out group-hover:opacity-100"
-                src="@/assets/img/information.svg"
-              />
+        <span class="m-auto"> Attack </span>
+      </template>
+      <template #tooltip>
+        <span> Lorem ipsum, dolor sit amet consectetur adipisicing. </span>
+      </template>
+      <template #content>
+        <div class="flex">
+          <ListItem input>
+            <template #title> Address: </template>
+            <template #item>
+              <input v-model="targetAddress" type="text" />
             </template>
-            <span
-              >Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, deserunt!</span
-            >
-          </Tooltip>
-          <span class="m-auto">Attack</span>
+            <span>{{ targetAddress }}</span>
+          </ListItem>
+          <img
+            class="cursor-pointer"
+            src="@/assets/img/check.svg"
+            alt="attack"
+            @click="onClickAttack()"
+          />
         </div>
       </template>
-      <div class="flex">
-        <ListItem input>
-          <template #title> Address: </template>
-          <template #item>
-            <input v-model="targetAddress" type="text" />
-          </template>
-          <span>{{ targetAddress }}</span>
-        </ListItem>
-        <img
-          class="cursor-pointer"
-          src="@/assets/img/check.svg"
-          alt="attack"
-          @click="onClickAttack()"
-        />
-      </div>
     </Accordion>
-    <Accordion dropdown>
+    <Accordion dropdown tooltip>
       <template #title>
-        <div class="group flex">
-          <Tooltip class="self-center">
-            <template #tooltip-image>
-              <img
-                class="mr-1 h-3 w-3 cursor-pointer opacity-0 transition-opacity duration-500 ease-out group-hover:opacity-100"
-                src="@/assets/img/information.svg"
+        <span class="m-auto"> Move </span>
+      </template>
+      <template #tooltip>
+        <span> Lorem ipsum, dolor sit amet consectetur adipisicing. </span>
+      </template>
+
+      <template #content>
+        <div class="flex">
+          <ListItem input>
+            <template #title> Direction: </template>
+            <template #item>
+              <SidebarDropdown
+                @selected="(item: string) => onDropdownChanged(item)"
+                :dropdown-items="enumKeys(Direction)"
               />
             </template>
-            <span
-              >Lorem ipsum dolor sit amet consectetur adipisicing elit.
-              Aspernatur, deserunt!</span
-            >
-          </Tooltip>
-          <span class="m-auto">Move</span>
+          </ListItem>
+          <img
+            class="cursor-pointer"
+            src="@/assets/img/check.svg"
+            alt="move"
+            @click="onClickMove()"
+          />
         </div>
       </template>
-      <div class="flex">
-        <ListItem input>
-          <template #title> Direction: </template>
-          <template #item>
-            <SidebarDropdown
-              @selected="(item: string) => onDropdownChanged(item)"
-              :dropdown-items="enumKeys(Direction)"
-            />
-          </template>
-        </ListItem>
-        <img
-          class="cursor-pointer"
-          src="@/assets/img/check.svg"
-          alt="move"
-          @click="onClickMove()"
-        />
-      </div>
     </Accordion>
   </div>
 </template>
 
 <script setup lang="ts">
 import ListItem from '~/components/sidebar-items/ListItem.vue'
-import Tooltip from '~/components/Tooltip.vue'
 import { useUserGameStore } from '~/stores/userGame'
 import { Direction } from '~/enums'
 
