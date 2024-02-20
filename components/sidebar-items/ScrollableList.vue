e3e3b8809aa7865576d8ffe86286b00247991bc5
handanyolak <handanyolak@gmail.com>
2024-02-20 14:33:53 +0300

    style(ScrollableList): Refactor ScrollableList.vue component

--- components/sidebar-items/ScrollableList.vue
+++ components/sidebar-items/ScrollableList.vue
@@ -1,17 +1,32 @@
 <template>
   <div class="max-h-32 overflow-y-auto overflow-x-hidden">
-    <ListItem v-for="(item, index) in items" :key="index" :copiable="copiable" :copy-value="copyValue[index]">
+    <ListItem
+      v-for="(item, index) in items"
+      :key="index"
+      :copiable="copiable"
+      :copy-value="copyValue[index]"
+    >
       <span v-if="linkable">
-        <a :href="item" target="_blank" class="flex justify-center items-center">{{ item }}
-          <img src="@/assets/img/link-external.svg" class="h-3 w-3 ml-1">
+        <a :href="item" target="_blank" class="flex items-center justify-center"
+          >{{ item }}
+          <img src="@/assets/img/link-external.svg" class="ml-1 h-3 w-3" />
         </a>
       </span>
       <span v-else>{{ item }}</span>
-      <AppButton v-if="actionable" @click="actionValue?.action(copyValue[index], item)">{{ actionValue?.name }}
-      </AppButton>
+      <template #action>
+        <AppButton
+          v-if="actionable"
+          @click="actionValue?.action(copyValue[index], item)"
+          basicHover
+          class="w-[120px]"
+          >{{ actionValue?.name }}
+        </AppButton>
+      </template>
       <template #tooltip>
-        <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
-          amet.</span>
+        <span
+          >Lorem ipsum dolor, sit amet consectetur adipisicing elit. Illum,
+          amet.</span
+        >
       </template>
     </ListItem>
   </div>
