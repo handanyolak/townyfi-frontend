67ed6ec55ff04d6457919302e69040d8119d658f
handanyolak <handanyolak@gmail.com>
2024-02-22 01:51:37 +0300

    feat(Map): Integrate MapNavigation toggle and outside click behavior

--- components/Map.vue
+++ components/Map.vue
@@ -4,48 +4,62 @@
       ref="mapElement"
       @wheel="onWheel($event)"
       v-if="hasMetamask && onValidNetwork"
-      class="relative z-50"
+      class="relative z-50 outline-none"
       tabindex="0"
       @keyup.up="navigateByArrowKeys(NavigateDirection.Up)"
       @keyup.right="navigateByArrowKeys(NavigateDirection.Right)"
       @keyup.down="navigateByArrowKeys(NavigateDirection.Down)"
       @keyup.left="navigateByArrowKeys(NavigateDirection.Left)"
     >
+      <button
+        ref="toggleButton"
+        @click="handleNavigationToggle"
+        :class="[
+          'absolute -top-1 -right-6 cursor-pointer transition-all  ease-in-out',
+          isMapNavigationVisible
+            ? 'translate-x-48 delay-300 duration-500 '
+            : 'duration-300',
+        ]"
+      >
+        <Icon
+          :class="[
+            ' transform text-3xl text-towni-brown-dark-400 transition-transform duration-200 ease-in-out',
+            isMapNavigationVisible ? 'rotate-180' : '',
+          ]"
+          name="material-symbols-light:double-arrow"
+        />
+      </button>
+
       <div
         class="relative grid overflow-hidden border-[10px] border-[#391f05]"
         :style="`grid-template-columns: repeat(${getGridColsByNearLevel}, minmax(0, 1fr));`"
       >
         <div
           class="absolute top-0 h-full w-full bg-map bg-cover"
-          :style="`transform: scale(${
-            100 + (maxNearLevel - nearLevel) * 10
-          }%); background-position: ${
-            (-originCoordinate._x *
-              BigInt(Math.round(width / (nearLevel * 2 + 1)))) %
-            BigInt(MAX_PIXEL_VALUE)
-          }px ${
-            (originCoordinate._y *
-              BigInt(Math.round(width / (nearLevel * 2 + 1)))) %
-            BigInt(MAX_PIXEL_VALUE)
-          }px`"
+          :style="mapStyle"
         />
         <Mapbox
           v-for="(item, index) in addressesByCoordinate"
           :key="index"
           :item="item"
+          v-memo="[item]"
           :emit-ready-event="index === 0"
           class="select-none"
           @dblclick="setModalInfo('MapboxModal', { coordinate: item })"
         />
       </div>
     </div>
-    <MapNavigation :height="width.toString()" />
+    <MapNavigation
+      ref="navigation"
+      :is-map-navigation-visible="isMapNavigationVisible"
+      :height="width.toString()"
+    />
   </div>
 </template>
 
 <script setup lang="ts">
 import { MAX_PIXEL_VALUE } from '~/constants'
-import { useDebounceFn } from '@vueuse/core'
+import { useDebounceFn, onClickOutside } from '@vueuse/core'
 import { useDrag } from '@vueuse/gesture'
 import MapNavigation from '~/components/MapNavigation.vue'
 import { NavigateDirection } from '~~/enums'
@@ -71,10 +85,28 @@ const { maxNearLevel } = useRuntimeConfig().public
 const mapElement = ref(null)
 const zoomOut = ref(0)
 const zoomIn = ref(0)
+const isMapNavigationVisible = ref(false)
+const navigation = ref<HTMLElement | null>(null)
+const toggleButton = ref<HTMLElement | null>(null)
 const { width } = useElementSize(mapElement)
 
 //--------[ Computed ]--------//
 const getGridColsByNearLevel = computed(() => nearLevel.value * 2 + 1)
+
+const mapStyle = computed(() => {
+  return {
+    transform: `scale(${100 + (maxNearLevel - nearLevel.value) * 10}%)`,
+    backgroundPosition: `${
+      (-originCoordinate.value._x *
+        BigInt(Math.round(width.value / (nearLevel.value * 2 + 1)))) %
+      BigInt(MAX_PIXEL_VALUE)
+    }px ${
+      (-originCoordinate.value._y *
+        BigInt(Math.round(width.value / (nearLevel.value * 2 + 1)))) %
+      BigInt(MAX_PIXEL_VALUE)
+    }px`,
+  }
+})
 
 //--------[ Methods ]--------//
 const debouncedSetUserCoordinate = useDebounceFn((x: bigint, y: bigint) => {
@@ -111,26 +143,30 @@ const onWheel = (event: WheelEvent) => {
 }
 
 const navigateByArrowKeys = (direction: NavigateDirection) => {
-  // TODO:Ethers try this
+  // TODO: Ethers try this
   let { _x, _y } = originCoordinate.value
 
   const navigateValue = 1n
-  if (direction === NavigateDirection.Up) {
-    _y += navigateValue
-  } else if (direction === NavigateDirection.Right) {
-    _x += navigateValue
-  } else if (direction === NavigateDirection.Down) {
-    _y -= navigateValue
-  } else if (direction === NavigateDirection.Left) {
-    _x -= navigateValue
-  } else {
-    // TODO: Throw exception etc.
+
+  switch (direction) {
+    case NavigateDirection.Up:
+      _y += navigateValue
+      break
+    case NavigateDirection.Right:
+      _x += navigateValue
+      break
+    case NavigateDirection.Down:
+      _y -= navigateValue
+      break
+    case NavigateDirection.Left:
+      _x -= navigateValue
+      break
+    default:
+      console.error('Invalid direction:', direction)
+      break
   }
 
-  setUserCoordinate({
-    _x,
-    _y,
-  })
+  setUserCoordinate({ _x, _y })
 }
 
 const dragHandler = async ({
@@ -153,5 +189,19 @@ const dragHandler = async ({
 useDrag(dragHandler, {
   domTarget: mapElement,
   filterTaps: true,
+})
+
+const handleNavigationToggle = () => {
+  isMapNavigationVisible.value = !isMapNavigationVisible.value
+}
+
+onClickOutside(navigation, (event) => {
+  if (
+    !toggleButton.value ||
+    toggleButton.value.contains(event.target as Node)
+  ) {
+    return
+  }
+  isMapNavigationVisible.value = false
 })
 </script>
