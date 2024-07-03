df6e3c202b2b509ef902a5796f5390f2429fce02
handanyolak <handanyolak@gmail.com>
2024-07-03 03:46:26 +0300

    refactor(ChatBox): Update ChatBox component UI and functionality

--- components/chatAndLog/ChatBox.vue
+++ components/chatAndLog/ChatBox.vue
@@ -1,13 +1,14 @@
 <template>
   <div>
-    <div ref="chatArea" class="h-56 overflow-auto p-4">
+    <div ref="chatArea" class="h-56 overflow-y-auto overflow-x-hidden p-4">
       <div
         v-for="(message, index) in chatMessages"
         ref="date"
         :key="index"
         :class="[
-          'relative my-1 flex items-end',
+          'my-2 flex items-end',
           { 'justify-end': message.author === address },
+          { group: isTooltip },
         ]"
       >
         <img
@@ -15,22 +16,39 @@
           class="mr-1 h-8 w-8 rounded-full"
           :src="makeBlockie(message.author)"
         />
+
         <div
           :class="[
-            'message-box relative z-10  min-w-14 p-1 text-xs',
+            'message-box relative z-10 min-w-14 break-words p-1 text-xs',
             {
               'rounded-bl-lg rounded-tl-lg rounded-tr-lg bg-towni-brown-dark-300 text-white':
                 message.author === address,
-              'rounded-br-lg rounded-tl-lg rounded-tr-lg bg-towni-brown-light-200':
+              'rounded-br-lg rounded-tl-lg rounded-tr-lg bg-[#a1866b] text-white':
                 message.author !== address,
             },
           ]"
         >
-          <p class="rounded-sm p-0.5 text-xs italic text-yellow-400">
-            {{ message.name }}
+          <div class="flex items-center space-x-1">
+            <p class="rounded-sm p-0.5 text-xs italic text-[#ffe186]">
+              {{ message.name }}
+            </p>
+            <p class="group text-xs italic text-[#ffe186]">
+              <span> {{ `(${middleCropping(message.author)})` }}</span>
+              <Tooltip
+                :icon-name="copied ? 'uil:check-circle' : 'uil:copy'"
+                icon-classes="text-yellow-400"
+                auto-close
+                @action="copied || copy(message.author)"
+              >
+                <span> Copied! </span>
+              </Tooltip>
+            </p>
+          </div>
+          <p class="mb-1 text-sm font-semibold">
+            {{ message.body }}
           </p>
-          <p class="mb-1 text-sm font-semibold">{{ message.body }}</p>
-          <span class="float-right ml-10 text-[10px] italic">{{
+
+          <span class="float-right text-[10px] italic">{{
             formattedDate(message.date)
           }}</span>
         </div>
@@ -43,28 +61,35 @@
     </div>
 
     <div
-      class="flex h-16 items-center border-t border-towni-brown-light-200 p-2"
+      class="flex h-20 items-start border-t border-towni-brown-light-200 p-2"
     >
       <div class="mx-2 w-full">
-        <input
-          v-model="youMessage"
-          class="bg-towni-brown-light-4000 w-full rounded-lg px-2 py-1 text-xs shadow-lg shadow-towni-brown-light-500 outline-none transition-all duration-300 ease-out focus:shadow-xl"
-          type="text"
-          placeholder="Your message here..."
-          autofocus
-          @keyup.enter="sendMessagee()"
-        />
+        <VeeForm class="flex flex-col items-center pt-1">
+          <VeeField
+            v-model="youMessage"
+            name="name"
+            autofocus
+            :rules="messageRules"
+            placeholder="Your message here..."
+            class="bg-towni-brown-light-4000 w-full rounded-lg px-2 py-1 text-xs shadow-lg shadow-towni-brown-light-500 outline-none transition-all duration-300 ease-out focus:shadow-xl"
+            @keyup.enter="sendMessage()"
+          />
+          <VeeErrorMessage class="text-xs text-red-600" name="name" />
+        </VeeForm>
       </div>
 
-      <div>
-        <button
-          class="inline-flex rounded-full p-1 outline-none hover:bg-towni-brown-light-400"
-          type="button"
-          @click="sendMessagee()"
-        >
-          <img class="h-5 w-7" src="@/assets/img/send-alt.svg" />
-        </button>
-      </div>
+      <button
+        class="inline-flex rounded-full p-1 outline-none hover:bg-towni-brown-light-400"
+        type="button"
+        @click="sendMessage()"
+      >
+        <Icon
+          :name="
+            isLoading ? 'svg-spinners:tadpole' : 'mingcute:send-plane-fill'
+          "
+          class="h-5 w-5 text-towni-brown-dark-300"
+        />
+      </button>
     </div>
   </div>
 </template>
@@ -73,7 +98,10 @@
 import { stringToHex } from 'viem'
 import { ref, nextTick } from 'vue'
 import makeBlockie from 'ethereum-blockies-base64'
-import { formattedDate } from '~/utils/helper'
+import { useClipboard, useDateFormat, useNavigatorLanguage } from '@vueuse/core'
+import { getBytes32Rule } from '~/composables/useYupRules'
+import { middleCropping } from '~/utils'
+import Tooltip from '~/components/common/Tooltip.vue'
 
 const contractStore = useContractStore()
 const { getKtaGameChatCaller } = storeToRefs(contractStore)
@@ -84,26 +112,50 @@ const { chatMessages } = storeToRefs(gameChatStore)
 const userWalletStore = useUserWalletStore()
 const { address } = storeToRefs(userWalletStore)
 
+// --------[ Data ]-------- //
 const youMessage = ref('')
 const chatArea = ref<HTMLInputElement | null>(null)
-// --------[ Method ]-------- //
-const sendMessagee = async () => {
-  if (youMessage.value.trim() !== '') {
-    await getKtaGameChatCaller.value.callFunction({
-      type: 'write',
-      name: 'sendMessage',
-      args: [[stringToHex(youMessage.value, { size: 32 })]],
-    })
+const isTooltip = ref(false)
+const isLoading = ref(false)
+const { language } = useNavigatorLanguage()
+const messageRules = getBytes32Rule()
+const { copy, copied } = useClipboard({
+  legacy: true,
+})
 
-    youMessage.value = ''
-    nextTick(() => {
-      if (chatArea.value) {
-        chatArea.value.scrollTo({
-          top: chatArea.value.scrollHeight,
-          behavior: 'smooth',
-        })
-      }
-    })
+// --------[ Computed ]-------- //
+const formattedDate = computed(() => (date: Date) => {
+  const formatDate = useDateFormat(date, 'YYYY-MM-DD HH:mm', {
+    locales: language.value,
+  })
+  return formatDate.value
+})
+
+// --------[ Method ]-------- //
+const sendMessage = async () => {
+  isLoading.value = true
+  try {
+    if (youMessage.value.trim() !== '') {
+      await getKtaGameChatCaller.value.callFunction({
+        type: 'write',
+        name: 'sendMessage',
+        args: [[stringToHex(youMessage.value, { size: 32 })]],
+      })
+
+      youMessage.value = ''
+      nextTick(() => {
+        if (chatArea.value) {
+          chatArea.value.scrollTo({
+            top: chatArea.value.scrollHeight,
+            behavior: 'smooth',
+          })
+        }
+      })
+    }
+  } catch (error) {
+    console.error('Error sending message:', error)
+  } finally {
+    isLoading.value = false
   }
 }
 </script>
