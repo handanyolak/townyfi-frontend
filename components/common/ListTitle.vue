9b6dd527ba4ca8b16f6577e935503a35fe7dd39c
handanyolak <handanyolak@gmail.com>
2024-11-25 18:54:58 +0300

    refactor(ListTitle): Update styling to include text-shadow and adjust margins

--- components/common/ListTitle.vue
+++ components/common/ListTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="mt-6 mb-2 rounded-lg bg-towny-brown-dark-300 bg-opacity-20 p-1 text-center text-towny-brown-dark-300 backdrop-blur"
+    class="text-shadow my-8 rounded-lg bg-towny-brown-dark-300 bg-opacity-20 p-1 text-center text-lg font-bold text-[#b18a68] shadow-lg backdrop-blur md:text-xl"
   >
     <slot />
   </div>
