ea489d2f317f89179cd3ff7bff3ac6df03c7d7a5
handanyolak <handanyolak@gmail.com>
2024-11-26 05:17:38 +0300

    feat(TheHeader): Enhance header layout with MobileMenu and HamburgerButton components for improved navigation

--- components/layout/TheHeader.vue
+++ components/layout/TheHeader.vue
@@ -2,102 +2,130 @@
   <div
     class="absolute top-0 z-100 w-full bg-white shadow-lg shadow-towny-brown-light-100 dark:bg-[#0D1117] dark:shadow-[#171e28]"
   >
-    <div class="mx-5">
+    <div class="mx-1 md:mx-5">
       <div class="flex items-center justify-between">
-        <span
-          class="step-2 flex select-none items-center bg-gradient-to-r from-towny-brown-dark-400 via-towny-brown-dark-400 to-towny-brown-dark-200 bg-clip-text text-5xl font-extrabold text-transparent"
-          style="
-            font-family:
-              Pirata One,
-              sans-serif;
-          "
-        >
-          <img class="h-7 w-7" src="@/assets/img/paper-document.svg" />
-          TownyFi
-        </span>
-        <div class="flex items-center space-x-2">
-          <button class="step-5" @click="setModalInfo('SearchModal')">
-            <img
-              src="~/assets/img/search.svg"
-              class="h-14 w-14 cursor-pointer"
-              alt="search"
-            />
-          </button>
-          <div v-if="hasMetamask" class="flex justify-between py-5">
-            <div v-if="onValidNetwork">
-              <div v-if="isConnected" class="space-x-1">
-                <AppButton
-                  v-if="!isRegistered"
-                  border-hover
-                  @click="setModalInfo('RegisterModal')"
-                >
-                  Register
-                </AppButton>
-                <AppButton
-                  class="step-4"
-                  border-hover
-                  inline-class="group-hover:bg-towny-brown-light-100 group-hover:text-towny-brown-dark-200"
-                  @click="disconnectWeb3()"
-                >
-                  {{ $t('disconnect_wallet') }}
+        <img
+          class="z-50 w-36 select-none md:w-52"
+          src="@/assets/img/townyfi-logo.svg"
+          alt="townyfi-logo"
+        />
+        <div class="flex items-center justify-between">
+          <div class="flex items-center space-x-2">
+            <button class="step-5" @click="setModalInfo('SearchModal')">
+              <img
+                src="~/assets/img/search.svg"
+                class="h-14 w-14 cursor-pointer"
+                alt="search"
+              />
+            </button>
+            <div v-if="hasMetamask" class="hidden justify-between py-5 md:flex">
+              <div v-if="onValidNetwork">
+                <div v-if="isConnected" class="space-x-1">
+                  <AppButton
+                    v-if="!isRegistered"
+                    border-hover
+                    @click="setModalInfo('RegisterModal')"
+                  >
+                    Register
+                  </AppButton>
+                  <AppButton
+                    class="step-4"
+                    border-hover
+                    inline-class="group-hover:bg-towny-brown-light-100 group-hover:text-towny-brown-dark-200"
+                    @click="disconnectWeb3()"
+                  >
+                    {{ $t('disconnect_wallet') }}
+                  </AppButton>
+                </div>
+                <AppButton v-else fill-hover @click="connectWeb3()">
+                  Connect Wallet
                 </AppButton>
               </div>
-              <AppButton v-else fill-hover @click="connectWeb3()">
-                Connect Wallet
+              <AppButton
+                v-else
+                target="_blank"
+                border
+                border-hover
+                @click="switchOrAddNetwork()"
+              >
+                Switch Network
               </AppButton>
             </div>
             <AppButton
               v-else
+              class="absolute left-1/2 top-24 w-5/6 -translate-x-1/2 text-center md:w-3/6 lg:w-2/6"
+              :href="'https://metamask.io/download/'"
               target="_blank"
-              border
-              border-hover
-              @click="switchOrAddNetwork()"
+              fill-hover
             >
-              Switch Network
+              Install Metamask
             </AppButton>
-          </div>
-          <AppButton
-            v-else
-            :href="'https://metamask.io/download/'"
-            target="_blank"
-            fill-hover
-          >
-            Install Metamask
-          </AppButton>
-          <AppDropdown
-            :select="language"
-            :dropdown-items="languages"
-            :icon-names="languages"
-            class="dropdown-background step-3"
-            @selected="(item) => selected(item)"
-          />
-          <img
-            :src="themeIcon"
-            class="h-14 w-14 cursor-pointer"
-            @click="toggleTheme()"
-          />
+            <div class="hidden md:flex">
+              <AppDropdown
+                :select="language"
+                :dropdown-items="languages"
+                :icon-names="languages"
+                class="dropdown-background step-3"
+                @selected="(item) => selected(item)"
+              />
+              <img
+                :src="themeIcon"
+                class="h-14 w-14 cursor-pointer"
+                @click="toggleTheme()"
+              />
 
-          <img
-            :src="audioIcon"
-            class="step-1 h-16 w-16 cursor-pointer"
-            @click="toggleAudio()"
-          />
-          <client-only>
-            <Vue3Lottie
-              v-if="audio"
-              class="cursor-pointer"
-              :animation-data="Harp"
-              :height="55"
-              :width="55"
-              :scale="1.4"
-              :auto-play="isAnimating"
-              :pause-animation="!isAnimating"
-              @click="toggleMusicAndAnimation()"
+              <img
+                :src="audioIcon"
+                class="step-1 h-16 w-16 cursor-pointer"
+                @click="toggleAudio()"
+              />
+              <client-only>
+                <Vue3Lottie
+                  v-if="audio"
+                  class="cursor-pointer"
+                  :animation-data="Harp"
+                  :height="55"
+                  :width="55"
+                  :scale="1.4"
+                  :auto-play="isAnimating"
+                  :pause-animation="!isAnimating"
+                  @click="toggleMusicAndAnimation()"
+                />
+              </client-only>
+            </div>
+          </div>
+          <div class="relative flex md:hidden">
+            <HamburgerButton
+              :is-open="menuOpen"
+              button-class="bg-towny-brown-dark-100"
+              @toggle="toggleMenu"
             />
-          </client-only>
+
+            <MobileMenu
+              v-model="menuOpen"
+              :menu-open="menuOpen"
+              :has-metamask="hasMetamask"
+              :on-valid-network="onValidNetwork"
+              :is-connected="isConnected"
+              :is-registered="isRegistered"
+              :language="language"
+              :languages="languages"
+              :selected="selected"
+              :audio="audio"
+              :theme-icon="themeIcon"
+              :audio-icon="audioIcon"
+              :toggle-theme="toggleTheme"
+              :toggle-audio="toggleAudio"
+              :toggle-music-and-animation="toggleMusicAndAnimation"
+              :is-animating="isAnimating"
+              :switch-or-add-network="switchOrAddNetwork"
+              @close-menu="menuOpen = false"
+            />
+          </div>
         </div>
       </div>
     </div>
+
     <AppTour :steps="steps" />
   </div>
 </template>
@@ -106,6 +134,8 @@
 import { useDark, useToggle } from '@vueuse/core'
 import { Vue3Lottie } from 'vue3-lottie'
 import AppDropdown from '~/components/common/AppDropdown.vue'
+import MobileMenu from '~/components/MobileMenu.vue'
+import HamburgerButton from '~/components/HamburgerButton.vue'
 import AppButton from '~/components/common/AppButton.vue'
 import AppTour from '~/components/AppTour.vue'
 import { $t } from '~/composables/useLang'
@@ -132,7 +162,6 @@ const { isRegistered } = storeToRefs(userGameStore)
 const { audio } = storeToRefs(appOptionStore)
 const { language } = storeToRefs(useUserOptions)
 
-const isAnimating = ref(false)
 useTour()
 
 // --------[ Composable ]-------- //
@@ -147,6 +176,8 @@ const search = ref('')
 const isValid = ref(false)
 const searchRules = getAddressRule()
 const toggleTheme = useToggle(isDark)
+const menuOpen = ref(false)
+const isAnimating = ref(false)
 
 const steps: Step[] = [
   {
@@ -224,14 +255,8 @@ const toggleMusicAndAnimation = () => {
   toggleMusic()
   isAnimating.value = !isAnimating.value
 }
-</script>
 
-<style scoped>
-/* TODO: temporarily font */
-@import url('https://fonts.googleapis.com/css?family=Pirata+One|Bilbo+Swash+Caps&display=swap');
-
-@font-face {
-  font-family: Morris;
-  src: url(https://cdn.statically.io/gh/EmmesCodes/Tipografias/dae9f5bb/MorrisInitials.ttf);
+const toggleMenu = () => {
+  menuOpen.value = !menuOpen.value
 }
-</style>
+</script>
