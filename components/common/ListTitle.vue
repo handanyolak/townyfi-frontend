5308406e4794d35117b81a819d19e746125a8b64
Handan Yolak <handanyolak@gmail.com>
2024-12-03 22:53:00 +0300

    fix(ListTitle): Adjust margin for improved layout consistency

--- components/common/ListTitle.vue
+++ components/common/ListTitle.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    class="text-shadow my-8 rounded-lg bg-towny-brown-dark-300 bg-opacity-20 p-1 text-center text-lg font-bold text-[#b18a68] shadow-lg backdrop-blur md:text-xl"
+    class="my-2 rounded-lg bg-towny-brown-dark-300 bg-opacity-20 p-1 text-center text-lg font-bold text-[#b18a68] shadow-lg backdrop-blur text-shadow md:text-xl"
   >
     <slot />
   </div>
