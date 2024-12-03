d191cb30b219f3104f233d6a62122531af72edb2
Handan Yolak <handanyolak@gmail.com>
2024-12-03 22:50:20 +0300

    feat(ReturnBackControls): Add ReturnBackControls component for recentering and teleporting functionality

---
+++ components/map/ReturnBackControls.vue
@@ -0,0 +1,94 @@
+<template>
+  <div
+    :style="size"
+    class="return-back flex flex-col items-center justify-center bg-towny-brown-dark-600 px-4 py-1"
+  >
+    <AppButton
+      class="my-2 w-full rounded-md bg-towny-brown-dark-400 text-towny-brown-light-400"
+      @click="goBackToInitialPosition()"
+    >
+      Recenter!
+    </AppButton>
+    <AppButton
+      v-if="hasUserTown && !isUserInTown"
+      class="my-2 w-full rounded-md bg-towny-brown-dark-400 text-towny-brown-light-400"
+      @click="teleportToTown()"
+    >
+      Teleport to Town!
+    </AppButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import AppButton from '~/components/common/AppButton.vue'
+
+// --------[ Store ]-------- //
+const appOptionsStore = useAppOptionsStore()
+const { clearModalInfo, setModalInfo } = appOptionsStore
+
+const userGameStore = useUserGameStore()
+const { setUserCoordinate } = userGameStore
+const { user, town } = storeToRefs(userGameStore)
+
+const contractStore = useContractStore()
+const { getKtaCaller } = storeToRefs(contractStore)
+
+// --------[ Prop & Emit ]-------- //
+defineProps({
+  size: {
+    type: Object,
+    required: true,
+  },
+})
+
+// --------[ Method ]-------- //
+
+const goBackToInitialPosition = () => {
+  setUserCoordinate({
+    _x: BigInt(user.value.coordinate._x.toString()),
+    _y: BigInt(user.value.coordinate._y.toString()),
+  })
+}
+
+const teleportToTown = async () => {
+  const confirmed = await setModalInfo('AnimationModal', {
+    animation: 'travel-map',
+    message: `Are you sure want to teleport to Town (${town.value.coordinate._x}, ${town.value.coordinate._y})?`,
+  })
+
+  if (!confirmed) {
+    return
+  }
+
+  try {
+    await getKtaCaller.value.callFunction({
+      type: 'write',
+      name: 'teleport',
+      args: [
+        [
+          {
+            _x: town.value.coordinate._x,
+            _y: town.value.coordinate._y,
+          },
+        ],
+      ],
+    })
+  } catch (error) {
+    console.error('Teleport transaction failed: ', error)
+  } finally {
+    clearModalInfo()
+  }
+}
+
+// --------[ Computed ]-------- //
+const hasUserTown = computed(() => user.value?.townInfo?.joinBlock !== 0n)
+
+const isUserInTown = computed(() => {
+  const userX = user.value?.coordinate?._x.toString()
+  const userY = user.value?.coordinate?._y.toString()
+  const townX = town.value?.coordinate?._x.toString()
+  const townY = town.value?.coordinate?._y.toString()
+
+  return userX === townX && userY === townY
+})
+</script>
