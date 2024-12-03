a0a845d99d76b0a0e7e760cc1f2e727bb3f3d7aa
Handan Yolak <handanyolak@gmail.com>
2024-12-04 01:30:18 +0300

    style(Unregistered): Enhance registration message styling for better visibility

--- components/tabs/common/Unregistered.vue
+++ components/tabs/common/Unregistered.vue
@@ -1,3 +1,7 @@
 <template>
-  <div>Register for the game to see this section.</div>
+  <div
+    class="my-10 text-center text-2xl font-bold text-towny-brown-dark-400 text-shadow"
+  >
+    Register for the game to see this section.
+  </div>
 </template>
