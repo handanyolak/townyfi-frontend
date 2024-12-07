05d578a9066ccd37a3d71fff95026d5bd7b2032f
Handan Yolak <handanyolak@gmail.com>
2024-12-07 21:47:53 +0300

    feat(Game.vue): Refactor settings rendering and improve title formatting

--- components/tabs/options/Game.vue
+++ components/tabs/options/Game.vue
@@ -1,16 +1,20 @@
 <template>
   <div>
-    <div v-for="(settingGroup, index) in settings" :key="index">
-      <ListItem
-        v-for="(value, key) in settingGroup"
-        :key="key"
-        :title="getTitle(index, key.toString())"
-        >{{ value }}</ListItem
-      >
+    <div v-for="(setting, parentKey) in settings" :key="parentKey">
+      <div v-if="typeof setting === 'object'">
+        <ListItem
+          v-for="(value, key) in setting"
+          :key="key"
+          :title="`${toCapitalizedWords(parentKey).toUpperCase()} ${toCapitalizedWords(key).toUpperCase()}`"
+          >{{ value }}</ListItem
+        >
+      </div>
+      <div v-else>
+        <ListItem :title="toCapitalizedWords(parentKey).toUpperCase()">{{
+          setting
+        }}</ListItem>
+      </div>
     </div>
-    <ListItem v-if="settings && settings[7]" title="Number Digits:">{{
-      settings[7]
-    }}</ListItem>
     <a href="https://docs.townyfi.com/" target="_blank">
       <AppButton class="my-6 flex w-full justify-center">
         <span>For more information, visit</span>
@@ -24,18 +28,7 @@
 import ListItem from '~/components/common/ListItem.vue'
 import AppButton from '~/components/common/AppButton.vue'
 
-const contractStore = useContractStore()
-const { getKta } = storeToRefs(contractStore)
-const settings = ref<any>(null)
+const userGameStore = useUserGameStore()
 
-onMounted(async () => {
-  settings.value = await getKta.value.read.settings()
-})
-
-const titles = ['Max', 'Price', 'Rate', 'Time', 'Min', 'Exp', 'Multiplier']
-
-const getTitle = (index: number, key: string): string => {
-  const prefix = titles[index] || 'Custom'
-  return `${prefix} ${toCapitalizedWords(key.toString())}:`
-}
+const { settings } = storeToRefs(userGameStore)
 </script>
