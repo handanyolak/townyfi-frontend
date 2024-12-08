be10497bded50958f9f095cab52628614edfb354
Handan Yolak <handanyolak@gmail.com>
2024-12-09 01:16:42 +0300

    style(ChatAndLogBox.vue, ChatBox.vue): Enhance chat UI with improved layouts and responsiveness

--- components/chatAndLog/ChatAndLogBox.vue
+++ components/chatAndLog/ChatAndLogBox.vue
@@ -3,33 +3,41 @@
     <div
       ref="chatBox"
       :class="[
-        isChat ? 'max-h-96' : 'max-h-9',
-        'ease fixed bottom-0 right-0 z-90 flex w-full flex-col transition-all duration-700 md:w-80',
+        isChat ? 'max-h-[700px]' : 'max-h-9',
+        'ease fixed bottom-0 right-0 z-90 flex w-full flex-col transition-all duration-700 md:w-[400px]',
       ]"
     >
       <div
-        class="grid h-10 w-full grid-cols-2 gap-x-1 rounded-t-2xl border border-b border-towny-brown-light-300 bg-towny-brown-light-300 p-2 shadow-md"
+        class="grid h-10 w-full grid-cols-2 gap-x-1 rounded-t-2xl border border-b border-towny-brown-light-300 bg-towny-brown-light-300 p-2 shadow-2xl"
       >
-        <button class="chat-tab-button" @click="toggleChatTabs(1)">
+        <button
+          :class="['chat-tab-button', tabClasses(1)]"
+          @click="toggleChatTabs(1)"
+        >
           Chat Box
         </button>
-        <button class="chat-tab-button" @click="toggleChatTabs(2)">Logs</button>
+        <button
+          :class="['chat-tab-button', tabClasses(2)]"
+          @click="toggleChatTabs(2)"
+        >
+          Logs
+        </button>
       </div>
 
-      <div class="h-72">
+      <div class="h-[350px]">
         <div
           :class="[
             'h-full overflow-auto bg-towny-brown-light-300',
-            { hidden: openChatTab !== 1, block: openChatTab === 1 },
+            tabVisibilityClass(1),
           ]"
         >
-          <ChatBox />
+          <ChatBox :is-chat="isChat" />
         </div>
 
         <div
           :class="[
             'h-full overflow-auto bg-towny-brown-light-300',
-            { hidden: openChatTab !== 2, block: openChatTab === 2 },
+            tabVisibilityClass(2),
           ]"
         >
           <LogBox />
@@ -49,6 +57,26 @@ const isChat = ref(false)
 const chatBox = ref(null)
 const openChatTab = ref(1)
 
+// --------[ Computed ]-------- //
+const tabClasses = computed(() => {
+  return (tabNumber: number) => {
+    if (!isChat.value || (isChat.value && openChatTab.value !== tabNumber)) {
+      return 'bg-towny-brown-dark-100'
+    }
+    if (isChat.value && openChatTab.value === tabNumber) {
+      return 'bg-towny-brown-dark-400'
+    }
+    return ''
+  }
+})
+
+const tabVisibilityClass = computed(() => {
+  return (tabNumber: number) => ({
+    hidden: openChatTab.value !== tabNumber,
+    block: openChatTab.value === tabNumber,
+  })
+})
+
 // --------[ Method ]-------- //
 const toggleChatTabs = (tabNumber: number) => {
   if (!isChat.value) {
@@ -67,8 +95,8 @@ const toggleChat = () => {
 onClickOutside(chatBox, () => (isChat.value = false))
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 .chat-tab-button {
-  @apply rounded-t-xl bg-towny-brown-dark-100 px-2 py-1 text-sm font-medium text-white outline-none transition-all duration-300 ease-out hover:bg-[#c99f75];
+  @apply rounded-t-xl px-2 py-1 text-sm font-medium text-white outline-none transition-all duration-300 ease-out hover:bg-towny-brown-dark-400;
 }
 </style>
--- components/chatAndLog/ChatBox.vue
+++ components/chatAndLog/ChatBox.vue
@@ -1,38 +1,38 @@
 <template>
-  <div>
-    <div ref="chatArea" class="h-56 overflow-y-auto overflow-x-hidden p-4">
+  <div class="flex h-full flex-col justify-end">
+    <div ref="chatArea" class="h-[350px] overflow-y-auto overflow-x-hidden p-4">
       <div
         v-for="(message, index) in chatMessages"
         ref="date"
         :key="index"
         :class="[
-          'my-2 flex items-end',
+          'my-3 flex items-end',
           { 'justify-end': message.author === address },
           { group: isTooltip },
         ]"
       >
         <img
           v-if="message.author !== address"
-          class="mr-1 h-8 w-8 rounded-full border border-towny-brown-dark-200"
+          class="mr-3 h-8 w-8 rounded-full border border-towny-brown-dark-200"
           :src="makeBlockie(message.author)"
         />
 
         <div
           :class="[
-            'message-box relative z-10 min-w-14 break-words p-1 text-xs',
+            'message-box relative z-10 w-60 break-words px-5 py-2 text-xs shadow-lg',
             {
-              'rounded-bl-lg rounded-tl-lg rounded-tr-lg bg-towny-brown-dark-300 text-white':
+              'rounded-bl-full rounded-tl-full rounded-tr-full bg-towny-brown-dark-300 text-white':
                 message.author === address,
-              'rounded-br-lg rounded-tl-lg rounded-tr-lg bg-[#a1866b] text-white':
+              'rounded-br-full rounded-tl-full rounded-tr-full bg-[#a1866b] text-white':
                 message.author !== address,
             },
           ]"
         >
           <div class="flex items-center space-x-1">
-            <p class="rounded-sm p-0.5 text-xs italic text-[#ffe186]">
+            <p class="rounded-sm p-0.5 text-xs font-bold italic text-[#6d4c41]">
               {{ message.name }}
             </p>
-            <p class="group text-xs italic text-[#ffe186]">
+            <p class="group text-xs italic text-[#6d4c41]">
               <span> {{ `(${middleCropping(message.author)})` }}</span>
               <Tooltip
                 :icon-name="copied ? 'uil:check-circle' : 'uil:copy'"
@@ -47,21 +47,19 @@
           <p class="mb-1 text-sm font-semibold">
             <span v-html="message.body" />
           </p>
-          <span class="float-right text-[10px] italic">{{
+          <span class="float-right text-xs italic text-white">{{
             formattedDate(message.date)
           }}</span>
         </div>
         <img
           v-if="message.author === address"
-          class="ml-1 h-8 w-8 rounded-full border border-towny-brown-dark-200"
+          class="ml-3 h-8 w-8 rounded-full border border-towny-brown-dark-200"
           :src="makeBlockie(message.author)"
         />
       </div>
     </div>
 
-    <div
-      class="flex h-20 items-start border-t border-towny-brown-light-200 p-2"
-    >
+    <div class="flex items-center border-t border-towny-brown-light-200 p-2">
       <div class="mx-2 w-full">
         <VeeForm class="flex flex-col items-center pt-1">
           <VeeField
@@ -70,16 +68,21 @@
             autocomplete="off"
             autofocus
             :rules="messageRules"
-            placeholder="Your message here..."
-            class="bg-towny-brown-light-4000 w-full rounded-lg px-2 py-1 text-xs shadow-lg shadow-towny-brown-light-500 outline-none transition-all duration-300 ease-out focus:shadow-xl"
+            placeholder="Hey, @You can send a message here!"
+            class="h-8 w-full rounded-lg bg-towny-brown-light-100 px-2 py-1 text-xs shadow-lg shadow-towny-brown-light-500 outline-none transition-all duration-300 ease-out focus:shadow-xl"
+            validate-on-input
             @keyup.enter="sendMessage()"
           />
-          <VeeErrorMessage class="text-xs text-red-600" name="name" />
+          <VeeErrorMessage
+            :class="isChat ? 'apacity-100 delay-200' : 'opacity-0'"
+            class="bg-error-red absolute bottom-14 left-1/2 z-10 w-[95%] -translate-x-1/2 rounded-lg p-4 px-2 text-center text-sm font-semibold text-white shadow-lg transition-opacity duration-300 ease-in-out"
+            name="name"
+          />
         </VeeForm>
       </div>
 
       <button
-        class="inline-flex rounded-full p-1 outline-none hover:bg-towny-brown-light-400"
+        class="inline-flex rounded-full p-1 outline-none transition-all duration-200 ease-in-out hover:scale-110 hover:bg-towny-brown-light-400"
         type="button"
         @click="sendMessage()"
       >
@@ -87,7 +90,7 @@
           :name="
             isLoading ? 'svg-spinners:tadpole' : 'mingcute:send-plane-fill'
           "
-          class="h-5 w-5 text-towny-brown-dark-300"
+          class="h-5 w-5 text-towny-brown-dark-300 transition-all duration-200 ease-in-out hover:scale-110"
         />
       </button>
     </div>
@@ -111,6 +114,10 @@ const { chatMessages } = storeToRefs(gameChatStore)
 
 const userWalletStore = useUserWalletStore()
 const { address } = storeToRefs(userWalletStore)
+
+defineProps<{
+  isChat?: boolean
+}>()
 
 // --------[ Data ]-------- //
 const youMessage = ref('')
