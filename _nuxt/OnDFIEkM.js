9c3095fb72fae1e1180b28c491d912850121b822
emretepedev <emretepedev@users.noreply.github.com>
2024-12-17 12:21:19 +0000

    deploy: 267ce2fbe3a940481552eabe9c2f89ff37e462f8

--- _nuxt/OnDFIEkM.js
+++
@@ -1,1 +0,0 @@
-import{a as t,b as o}from"./CLggBwCL.js";import{d as r,t as s,v as l,ay as n}from"./BQdbJqYC.js";const i=r({__name:"default",setup(u){const e=t({selector:"body",storageKey:"theme",valueDark:"dark",valueLight:"light"});return o(e),(a,c)=>(s(),l("div",null,[n(a.$slots,"default")]))}});export{i as default};
