366c0ffdf97968115aca5ffb201976f400d3f4f7
emretepedev <emretepedev@users.noreply.github.com>
2025-01-08 16:51:55 +0000

    deploy: b613a450e63131150c65b426e29c3c4c05461cc8

--- _nuxt/Dd1xho1q.js
+++ _nuxt/Dd1xho1q.js
@@ -1,4 +1,4 @@
-import{dA as v,dB as k,cl as u,cs as i,cg as c,cC as y,cr as g,dC as p,cB as w,cF as b,dL as C,dK as x,cA as E,cf as N,f0 as A,dE as h,dD as L,cw as T}from"./D6S8hHek.js";const O=v`
+import{cy as v,cz as k,cA as u,cB as i,cC as c,cD as y,cE as g,cF as p,cG as C,cH as w,cI as b,bg as x,bd as E,cJ as N,cK as A,cL as L,cM as h,bi as T,cN as O}from"./h9ewbg75.js";const S=v`
   :host {
     z-index: var(--w3m-z-index);
     display: block;
@@ -35,7 +35,10 @@ import{dA as v,dB as k,cl as u,cs as i,cg as c,cC as y,cr as g,dC as p,cB as w,c
     animation: zoom-in 0.2s var(--wui-ease-out-power-2);
     animation-fill-mode: backwards;
     outline: none;
-    transition: border-radius var(--wui-duration-lg) var(--wui-ease-out-power-1);
+    transition:
+      border-radius var(--wui-duration-lg) var(--wui-ease-out-power-1),
+      background-color var(--wui-duration-lg) var(--wui-ease-out-power-1);
+    will-change: border-radius, background-color;
   }
 
   :host(.embedded) wui-card {
@@ -135,7 +138,7 @@ import{dA as v,dB as k,cl as u,cs as i,cg as c,cC as y,cr as g,dC as p,cB as w,c
       height: var(--new-height);
     }
   }
-`;var s=function(n,e,t,o){var d=arguments.length,a=d<3?e:o===null?o=Object.getOwnPropertyDescriptor(e,t):o,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")a=Reflect.decorate(n,e,t,o);else for(var m=n.length-1;m>=0;m--)(l=n[m])&&(a=(d<3?l(a):d>3?l(e,t,a):l(e,t))||a);return d>3&&a&&Object.defineProperty(e,t,a),a};const f="scroll-lock";let r=class extends k{constructor(){super(),this.unsubscribe=[],this.abortController=void 0,this.enableEmbedded=u.state.enableEmbedded,this.open=i.state.open,this.caipAddress=c.state.activeCaipAddress,this.caipNetwork=c.state.activeCaipNetwork,this.shake=i.state.shake,this.initializeTheming(),y.prefetch(),this.unsubscribe.push(i.subscribeKey("open",e=>e?this.onOpen():this.onClose()),i.subscribeKey("shake",e=>this.shake=e),c.subscribeKey("activeCaipNetwork",e=>this.onNewNetwork(e)),c.subscribeKey("activeCaipAddress",e=>this.onNewAddress(e))),g.sendEvent({type:"track",event:"MODAL_LOADED"})}firstUpdated(){u.setEnableEmbedded(this.enableEmbedded),this.enableEmbedded&&this.caipAddress&&i.close()}disconnectedCallback(){this.unsubscribe.forEach(e=>e()),this.onRemoveKeyboardListener()}render(){return this.style.cssText=`
+`;var s=function(n,e,t,o){var d=arguments.length,a=d<3?e:o===null?o=Object.getOwnPropertyDescriptor(e,t):o,l;if(typeof Reflect=="object"&&typeof Reflect.decorate=="function")a=Reflect.decorate(n,e,t,o);else for(var m=n.length-1;m>=0;m--)(l=n[m])&&(a=(d<3?l(a):d>3?l(e,t,a):l(e,t))||a);return d>3&&a&&Object.defineProperty(e,t,a),a};const f="scroll-lock";let r=class extends k{constructor(){super(),this.unsubscribe=[],this.abortController=void 0,this.enableEmbedded=u.state.enableEmbedded,this.open=i.state.open,this.caipAddress=c.state.activeCaipAddress,this.caipNetwork=c.state.activeCaipNetwork,this.shake=i.state.shake,this.initializeTheming(),y.prefetch(),this.unsubscribe.push(i.subscribeKey("open",e=>e?this.onOpen():this.onClose()),i.subscribeKey("shake",e=>this.shake=e),c.subscribeKey("activeCaipNetwork",e=>this.onNewNetwork(e)),c.subscribeKey("activeCaipAddress",e=>this.onNewAddress(e))),g.sendEvent({type:"track",event:"MODAL_LOADED"})}firstUpdated(){if(u.setEnableEmbedded(this.enableEmbedded),this.caipAddress){if(this.enableEmbedded){i.close();return}this.onNewAddress(this.caipAddress)}}disconnectedCallback(){this.unsubscribe.forEach(e=>e()),this.onRemoveKeyboardListener()}render(){return this.style.cssText=`
       --local-border-bottom-mobile-radius: ${this.enableEmbedded?"clamp(0px, var(--wui-border-radius-l), 44px)":"0px"};
     `,this.enableEmbedded?p`${this.contentTemplate()}
         <w3m-tooltip></w3m-tooltip> `:this.open?p`
@@ -145,6 +148,7 @@ import{dA as v,dB as k,cl as u,cs as i,cg as c,cC as y,cr as g,dC as p,cB as w,c
           <w3m-tooltip></w3m-tooltip>
         `:null}contentTemplate(){return p` <wui-card
       shake="${this.shake}"
+      data-embedded="${C(this.enableEmbedded)}"
       role="alertdialog"
       aria-modal="true"
       tabindex="0"
@@ -154,7 +158,7 @@ import{dA as v,dB as k,cl as u,cs as i,cg as c,cC as y,cr as g,dC as p,cB as w,c
       <w3m-router></w3m-router>
       <w3m-snackbar></w3m-snackbar>
       <w3m-alertbar></w3m-alertbar>
-    </wui-card>`}async onOverlayClick(e){e.target===e.currentTarget&&await this.handleClose()}async handleClose(){w.state.view==="UnsupportedChain"||await b.isSIWXCloseDisabled()?i.shake():i.close()}initializeTheming(){const{themeVariables:e,themeMode:t}=T.state,o=C.getColorTheme(t);x(e,o)}onClose(){this.open=!1,this.classList.remove("open"),this.onScrollUnlock(),E.hide(),this.onRemoveKeyboardListener()}onOpen(){this.open=!0,this.classList.add("open"),this.onScrollLock(),this.onAddKeyboardListener()}onScrollLock(){const e=document.createElement("style");e.dataset.w3m=f,e.textContent=`
+    </wui-card>`}async onOverlayClick(e){e.target===e.currentTarget&&await this.handleClose()}async handleClose(){w.state.view==="UnsupportedChain"||await b.isSIWXCloseDisabled()?i.shake():i.close()}initializeTheming(){const{themeVariables:e,themeMode:t}=O.state,o=x.getColorTheme(t);E(e,o)}onClose(){this.open=!1,this.classList.remove("open"),this.onScrollUnlock(),N.hide(),this.onRemoveKeyboardListener()}onOpen(){this.open=!0,this.classList.add("open"),this.onScrollLock(),this.onAddKeyboardListener()}onScrollLock(){const e=document.createElement("style");e.dataset.w3m=f,e.textContent=`
       body {
         touch-action: none;
         overflow: hidden;
@@ -163,4 +167,4 @@ import{dA as v,dB as k,cl as u,cs as i,cg as c,cC as y,cr as g,dC as p,cB as w,c
       w3m-modal {
         pointer-events: auto;
       }
-    `,document.head.appendChild(e)}onScrollUnlock(){const e=document.head.querySelector(`style[data-w3m="${f}"]`);e&&e.remove()}onAddKeyboardListener(){this.abortController=new AbortController;const e=this.shadowRoot?.querySelector("wui-card");e?.focus(),window.addEventListener("keydown",t=>{if(t.key==="Escape")this.handleClose();else if(t.key==="Tab"){const{tagName:o}=t.target;o&&!o.includes("W3M-")&&!o.includes("WUI-")&&e?.focus()}},this.abortController)}onRemoveKeyboardListener(){this.abortController?.abort(),this.abortController=void 0}async onNewAddress(e){const t=N.getPlainAddress(e);this.caipAddress=e,await b.initializeIfEnabled(),(!t||this.enableEmbedded)&&i.close()}onNewNetwork(e){if(!this.caipAddress){this.caipNetwork=e,w.goBack();return}const t=this.caipNetwork?.caipNetworkId?.toString(),o=e?.caipNetworkId?.toString();t&&o&&t!==o&&this.caipNetwork?.name!=="Unknown Network"&&w.goBack(),this.caipNetwork=e}};r.styles=O;s([A({type:Boolean})],r.prototype,"enableEmbedded",void 0);s([h()],r.prototype,"open",void 0);s([h()],r.prototype,"caipAddress",void 0);s([h()],r.prototype,"caipNetwork",void 0);s([h()],r.prototype,"shake",void 0);r=s([L("w3m-modal")],r);export{r as W3mModal};
+    `,document.head.appendChild(e)}onScrollUnlock(){const e=document.head.querySelector(`style[data-w3m="${f}"]`);e&&e.remove()}onAddKeyboardListener(){this.abortController=new AbortController;const e=this.shadowRoot?.querySelector("wui-card");e?.focus(),window.addEventListener("keydown",t=>{if(t.key==="Escape")this.handleClose();else if(t.key==="Tab"){const{tagName:o}=t.target;o&&!o.includes("W3M-")&&!o.includes("WUI-")&&e?.focus()}},this.abortController)}onRemoveKeyboardListener(){this.abortController?.abort(),this.abortController=void 0}async onNewAddress(e){const t=A.getPlainAddress(e);this.caipAddress=e,await b.initializeIfEnabled(),(!t||this.enableEmbedded)&&i.close()}onNewNetwork(e){if(!this.caipAddress){this.caipNetwork=e,w.goBack();return}const t=this.caipNetwork?.caipNetworkId?.toString(),o=e?.caipNetworkId?.toString();t&&o&&t!==o&&this.caipNetwork?.name!=="Unknown Network"&&w.goBack(),this.caipNetwork=e}};r.styles=S;s([L({type:Boolean})],r.prototype,"enableEmbedded",void 0);s([h()],r.prototype,"open",void 0);s([h()],r.prototype,"caipAddress",void 0);s([h()],r.prototype,"caipNetwork",void 0);s([h()],r.prototype,"shake",void 0);r=s([T("w3m-modal")],r);export{r as W3mModal};
---
+++ _nuxt/IbriqNAB.js
@@ -0,0 +1,1 @@
+import{_ as t,t as r,v as a,az as n}from"./h9ewbg75.js";const o={};function s(e,c){return r(),a("div",null,[n(e.$slots,"default")])}const _=t(o,[["render",s]]);export{_ as default};
